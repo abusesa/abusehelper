09f59ed0c1f61b5fa8bfaa4828a5457b46772c87
Joachim Viide <jviide@iki.fi>
2009-10-13 10:37:26 +0000

    Added a small experimental bot for throwing events to a CouchDB instance.

---
+++ thirdparty/couchbot.py
@@ -0,0 +1,33 @@
+import couchdb.client
+from idiokit import threado, events
+
+@threado.stream
+def events_to_couchdb(inner, url, db_name):
+    server = couchdb.client.Server(url)
+    try:
+        db = server.create(db_name)
+    except couchdb.client.PreconditionFailed:
+        db = server[db_name]
+
+    while True:
+        event = yield inner
+
+        doc = dict()
+        for key, values in event.attrs.items():
+            doc[key] = list(values)
+        db.create(doc)
+
+def main():
+    from idiokit.xmpp import XMPP
+
+    xmpp = XMPP("user@example.com", "password")
+    xmpp.connect()
+    room = xmpp.muc.join("room@conference.example.com", "couchbot")
+
+    pipeline = (room
+                | events.stanzas_to_events()
+                | events_to_couchdb("http://localhost:5984", "roomdb"))
+    for _ in pipeline: pass
+
+if __name__ == "__main__":
+    main()
