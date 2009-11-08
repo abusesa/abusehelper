5c0cb83a8f212f913e6caee6d16da60f15d64468
Joachim Viide <jviide@iki.fi>
2009-11-08 00:16:27 +0000

    Made events.py use the new stream_fast idiokit feature and added a minor optimization (events now cache their corresponding elements).

--- abusehelper/core/events.py
+++ abusehelper/core/events.py
@@ -11,15 +11,17 @@ class Event(object):
         if not element.named("event", EVENT_NS):
             return None
 
-        event = Event()
+        event = Event(element)
         for attr in element.children("attr").with_attrs("key", "value"):
             event.add(attr.get_attr("key"), attr.get_attr("value"))
         return event
 
-    def __init__(self):
+    def __init__(self, element=None):
         self.attrs = dict()
+        self._element = None
 
     def add(self, key, value):
+        self._element = None
         self.attrs.setdefault(key, set()).add(value)
 
     def contains_key_value(self, key, value):
@@ -29,36 +31,40 @@ class Event(object):
         return key in self.attrs
 
     def to_element(self):
-        event = Element("event", xmlns=EVENT_NS)
+        if self._element is None:
+            event = Element("event", xmlns=EVENT_NS)
 
-        for key, values in self.attrs.items():
-            for value in values:
-                attr = Element("attr", key=key, value=value)
-                event.add(attr)
-        return event
+            for key, values in self.attrs.items():
+                for value in values:
+                    attr = Element("attr", key=key, value=value)
+                    event.add(attr)
+            self._element = event
+        return self._element
 
-@threado.stream
+@threado.stream_fast
 def stanzas_to_events(inner):
     while True:
-        element = yield inner
+        yield inner
 
-        for child in element.children():
-            event = Event.from_element(child)
-            if event is not None:
-                inner.send(event)
+        for element in inner.iter():
+            for child in element.children():
+                event = Event.from_element(child)
+                if event is not None:
+                    inner.send(event)
 
-@threado.stream
+@threado.stream_fast
 def events_to_elements(inner, include_body=True):
     while True:
-        event = yield inner
+        yield inner
 
-        if include_body:
-            fields = list()
-            for key, values in event.attrs.items():
-                for value in values:
-                    fields.append("%s=%s" % (key, value))
-            body = Element("body")
-            body.text = "%s" % ", ".join(fields)
-            inner.send(body, event.to_element())
-        else:
-            inner.send(event.to_element())
+        for event in inner.iter():
+            if include_body:
+                fields = list()
+                for key, values in event.attrs.items():
+                    for value in values:
+                        fields.append("%s=%s" % (key, value))
+                body = Element("body")
+                body.text = "%s" % ", ".join(fields)
+                inner.send(body, event.to_element())
+            else:
+                inner.send(event.to_element())
