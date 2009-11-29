fd1a109f1718d672cdb49555e6c5e992569dc94f
Joachim Viide <jviide@iki.fi>
2009-11-29 17:09:32 +0000

    Added abusehelper.core.startup, a quick hack to try out launching AbuseHelper processes based on INI file sections. Run the script in the following way:  % python -m abusehelper.core.startup config.cfg where config.cfg is an INI file. startup will then launch a process per each section in config.cfg (not counting [DEFAULT]). For example, section named [abusehelper.core.mailer] launches abushelper.core.mailer, of course with the options defined in that section (and [DEFAULT]) as usual.

---
+++ abusehelper/core/startup.py
@@ -0,0 +1,52 @@
+import os
+import sys
+import time
+import errno
+import signal
+import subprocess
+import ConfigParser
+
+def all_running(processes):
+    for process in processes:
+        retval = process.poll()
+        if retval is not None:
+            return False
+    return True
+
+def kill_processes(processes, sig):
+    for process in processes:
+        try:
+            os.kill(process.pid, sig)
+        except OSError, ose:
+            if ose.errno != errno.ESRCH:
+                raise
+
+def main(config_filename):
+    conf_parser = ConfigParser.SafeConfigParser()
+
+    conf_file = open(config_filename, "r")
+    try:
+        conf_parser.readfp(conf_file)
+    finally:
+        conf_file.close()
+
+    processes = dict()
+    for section in conf_parser.sections():
+        process = subprocess.Popen([sys.executable, "-m", section,
+                                    "--ini-file", config_filename,
+                                    "--ini-section", section])
+        processes[section] = process
+
+    try:
+        while all_running(processes.values()):
+            time.sleep(0.1)
+    finally:
+        kill_processes(processes.values(), signal.SIGINT)
+        for _, process in processes.items():
+            process.wait()
+main.config_filename_help = ("launch processes based in this INI file, "+
+                             "one per section ([DEFAULT] section not included)")
+
+if __name__ == "__main__":
+    import opts
+    opts.optparse(main)
