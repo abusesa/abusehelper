9c7ea734ebaec8fe06da94b9ffd96093e5bb9ab6
Joachim Viide <jviide@iki.fi>
2009-12-07 12:11:30 +0000

    Removed gwiki referenced from the example config file and abusehelper.core.config. If needed it can be moved into its own place later.

--- abusehelper/core/config.py
+++ abusehelper/core/config.py
@@ -3,17 +3,6 @@ import csv
 from idiokit import threado, timer
 from abusehelper.core import events, services
 
-def event_rows(event):
-    result = dict()
-    result["addresses"] = event.attrs.get('Abuse email', set([''])).pop()
-    result["asn"] = event.attrs.get('ASN1', set([''])).pop()
-    result["AS Name"] = event.attrs.get('AS Name', set([''])).pop()
-    result["ip-range"] = ""
-    result["dshield addresses"] = ""
-    result["dshield template"] = "dshield_template_fi"
-    result["pgp"] = ""
-    return result
-
 def csv_rows(data):
     lines = [line for line in data.splitlines()
              if line.strip() and not line.strip().startswith("#")]
@@ -84,99 +73,6 @@ class ConfigFollower(threado.GeneratorStream):
             ranges = row["ip-range"]
             addresses = row["dshield addresses"] or row["addresses"]
             addresses = frozenset(x.strip() for x in addresses.split(","))
-            template = row["dshield template"]
-            pgp = row["pgp"]
-
-            if addresses and template:
-                if ranges:
-                    defaults.setdefault(asn, list()).append(ranges)
-
-                key = asn, ranges, addresses
-                if key in self.confs:
-                    item, _, _ = self.confs[key]
-                else:
-                    item = Config(asn, ranges, addresses)
-                confs[key] = item, template, pgp
-
-        update = set()
-        discard = set(self.confs[key][0] for key in set(self.confs)-set(confs))
-        for key, (item, template, pgp) in confs.iteritems():
-            asn, ranges, addresses = key
-
-            if not ranges:
-                default = ",".join(defaults.get(asn, list()))
-                if default:
-                    default = "not (" + default + ")"
-                if item.filter != default:
-                    update.add(item)
-                item.filter = default
-
-            if template != item.template:
-                item.template = template
-                update.add(item)
-
-            if pgp != item.pgp:
-                item.pgp = pgp
-                update.add(item)
-                
-            if key not in self.confs:
-                update.add(item)
-
-        self.confs = confs
-        return update, discard
-
-class WikiConfigFollower(ConfigFollower):
-    def __init__(self, url, username, password, search, **kw):
-        threado.GeneratorStream.__init__(self)
-
-        self.url = url
-        self.confs = dict()
-        self.username = username
-        self.password = password
-        self.search = search
-
-        self.start()
-
-    def run(self):
-        from opencollab.meta import Metas
-        from opencollab.util.wiki import getPages
-        from opencollab.wiki import GraphingWiki
-        from idiokit import timer
-
-        while True:
-            self.gwiki = GraphingWiki(self.url)
-            yield self.inner.thread(self.gwiki.authenticate, 
-                                    self.username, 
-                                    self.password)
-            data = yield self.inner.thread(getPages, self.gwiki, self.search)
-
-            all_events = list()
-
-            for page in data.keys():
-                event = events.Event()
-
-                for key, values in data[page].iteritems():
-                    for value in values:
-                        event.add(key, value)
-
-                all_events.append(event)
-
-            update, discard = self._update(all_events)
-
-            self.inner.send(update, discard)
-
-            timer.sleep(10.0)
-
-    def _update(self, events):
-        confs = dict()
-        defaults = dict()
-
-        for event in events:
-            row = event_rows(event)
-
-            asn = row["asn"]
-            ranges = row["ip-range"]
-            addresses = row["dshield addresses"] or row["addresses"]
             template = row["dshield template"]
             pgp = row["pgp"]
 
