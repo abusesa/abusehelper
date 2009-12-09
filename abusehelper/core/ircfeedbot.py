19d34647f97cdf089eab30374acf934c53282eef
Joachim Viide <jviide@iki.fi>
2009-12-09 12:02:49 +0000

    Turned ircfeedbot into a service.

--- abusehelper/core/ircfeedbot.py
+++ abusehelper/core/ircfeedbot.py
@@ -1,8 +1,7 @@
 import re
 
-from abusehelper.core import events
+from abusehelper.core import events, services, roomfarm
 from idiokit import threado, util
-from idiokit.xmpp import connect
 from idiokit.irc import IRC
 
 @threado.stream
@@ -41,9 +40,74 @@ def parse(inner):
 
         inner.send(event)
 
+class IRCFeedSession(services.Session):
+    def __init__(self, service):
+        services.Session.__init__(self)
+        self.service = service
+        self.previous = None
+
+    @threado.stream
+    def config(inner, self, conf):
+        self.dsts.dec(self.previous)
+
+        if conf is None:
+            self.service.rooms(self)
+            self.previous = None
+        else:
+            room = self.service.rooms(self, conf["room"])
+            self.dsts.inc(room)
+            self.previous = room
+
+        yield
+        inner.finish(conf)
+
+class IRCFeedService(roomfarm.RoomFarm):
+    def __init__(self, xmpp, 
+                 host, port, channel, own_nick, feed_nick, password, use_ssl):
+        roomfarm.RoomFarm.__init__(self, xmpp)
+
+        self.dsts = roomfarm.Counter()
+
+        self.host = host
+        self.port = port
+        self.channel = channel
+        self.own_nick = own_nick
+        self.feed_nick = feed_nick
+        self.password = password
+        self.use_ssl = use_ssl
+
+    @threado.stream_fast
+    def distribute(inner, self):
+        while True:
+            yield inner
+
+            for event in self.inner:
+                for room, _ in self.dsts:
+                    room.send(event)
+
+    @threado.stream
+    def handle_room(inner, self, name):
+        room = yield inner.sub(self.xmpp.muc.join(name))
+        yield inner.sub(events.events_to_elements()
+                        | room
+                        | threado.throws())
+
+    def run(self):
+        irc = IRC(self.host, self.port, ssl=self.use_ssl)
+        print "Connecting IRC server", self.host, "port", self.port
+        yield irc.connect(self.own_nick, password=self.password)
+        print "Connected IRC server", self.host, "port", self.port
+        irc.join(self.channel)
+        print "Joined IRC channel", self.channel
+
+        yield self.inner.sub(irc 
+                             | filter(self.channel, self.feed_nick) 
+                             | parse()
+                             | self.distribute())
+
 def main(xmpp_jid,
          xmpp_password,
-         xmpp_room,
+         service_room,
          irc_host,
          irc_channel,
          irc_port=6667, 
@@ -52,29 +116,33 @@ def main(xmpp_jid,
          irc_password=None, 
          irc_use_ssl=False,
          log_file=None):
+    import getpass
+    from idiokit.xmpp import connect
     from abusehelper.core import log
 
-    logger = log.config_logger("ircfeedbot", filename=log_file)
+    if not xmpp_password:
+        xmpp_password = getpass.getpass("XMPP password: ")
+
+    logger = log.config_logger("ircfeed", filename=log_file)
 
     @threado.stream
     def bot(inner):
-        irc = IRC(irc_host, irc_port, ssl=irc_use_ssl)
-        nick = yield irc.connect(irc_own_nick, password=irc_password)
-        irc.join(irc_channel)
-
+        print "Connecting XMPP server with JID", xmpp_jid
         xmpp = yield connect(xmpp_jid, xmpp_password)
-        room = yield xmpp.muc.join(xmpp_room, nick)
+        xmpp.core.presence()
 
-        yield inner.sub(irc 
-                        | filter(irc_channel, irc_feed_nick) 
-                        | parse() 
-                        | events.events_to_elements()
-                        | room 
-                        | threado.throws())
+        print "Joining lobby", service_room
+        lobby = yield services.join_lobby(xmpp, service_room, "ircfeed")
+        logger.addHandler(log.RoomHandler(lobby.room))
+
+        service = IRCFeedService(xmpp, irc_host, irc_port, 
+                                 irc_channel, irc_own_nick, irc_feed_nick, 
+                                 irc_password, irc_use_ssl)
+        yield inner.sub(lobby.offer("ircfeed", service))
     threado.run(bot())
 main.xmpp_jid_help = "the XMPP username (e.g. user@xmpp.example.com)"
 main.xmpp_password_help = "the XMPP password"
-main.xmpp_room_help = "the XMPP room where the feed is forwarded"
+main.service_room_help = "the room where the services are collected"
 main.irc_host_help = "the IRC server hostname"
 main.irc_channel_help = "the IRC feed channel"
 main.irc_feed_nick_help = "listen this IRC nick (default: listen everybody)"
