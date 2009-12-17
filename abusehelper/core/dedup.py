0a89484864bdff25aabf0b08bbc3e7b347c1f946
Joachim Viide <jviide@iki.fi>
2009-12-17 01:21:20 +0000

    Added the possibility to check whether a string is in a dedup filter already without adding it to the dedup filter.

--- abusehelper/core/dedup.py
+++ abusehelper/core/dedup.py
@@ -2,24 +2,30 @@ import struct
 import collections
 from hashlib import md5
 
-class Dedup(object):
-    unpack = struct.Struct("!Q").unpack
+unpack = struct.Struct("!Q").unpack
 
+def dedup_key(string):
+    return unpack(md5(string).digest()[:8])[0]
+
+class Dedup(object):
     def __init__(self, size=10**6):
         self.size = size
         self.set = set()
         self.queue = collections.deque()
 
+    def __contains__(self, string):
+        return dedup_key(string) in self.set
+
     def add(self, string):
-        hash = self.unpack(md5(string).digest()[:8])[0]
-        if hash in self.set:
+        key = dedup_key(string)
+        if key in self.set:
             return False
 
         if len(self.queue) >= self.size:
             first = self.queue.popleft()
             self.set.discard(first)
-        self.queue.append(hash)
-        self.set.add(hash)
+        self.queue.append(key)
+        self.set.add(key)
         return True
 
     def __getstate__(self):
