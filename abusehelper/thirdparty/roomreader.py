8c2b634e5c124d38259ad0e10a5a9dbc0bbad7f4
jani <janike@iki.fi>
2009-12-20 21:53:22 +0000

    Enhancement: abusehelper.thirdparty.roomreader. Exceptions are not lost anymore + prints also for joining and leaving rooms.

--- abusehelper/thirdparty/roomreader.py
+++ abusehelper/thirdparty/roomreader.py
@@ -1,5 +1,5 @@
-"""Roomreader is a simple client for reading messages in the room.
-Roomreader can print messages to stdout (default) or log to a file.
+"""Roomreader is a simple client for reading messages in multiple rooms.
+Roomreader can print messages to stdout (default) or to a log file.
 """
 
 from idiokit import threado, util
@@ -7,21 +7,47 @@ from idiokit.jid import JID
 
 
 @threado.stream
-def xmpp_to_stdout(inner,own_jid,debug):
+def xmpp_to_stdout(inner,own_jid,participants,debug):
     """Print messages received from room."""
+    in_room={}
+
+    for participant in  participants:
+        in_room[participant.name.resource] = True
+    
     while True:
         elements = yield inner
+        for message in elements.with_attrs("from"):
 
-        for message in elements.named("message").with_attrs("from"):
+            type=None
             sender = JID(message.get_attr("from"))
             if sender == own_jid:
                 continue
             if sender.resource is None:
                 continue
-            
+            sendertxt = "<%s@%s/%s>" % (sender.node, sender.domain, sender.resource)
+
+#            # Preserving this code for documentative purposes.
+#            # You've gotta love the logic behind type & status.
+#            for presence in message.named("presence").with_attrs("from"):
+#                type = presence.get_attr("type", None)
+#                for newstatus in presence.children('status'):
+#                    status = newstatus.text
+
+            if type == None and sender.resource not in in_room:
+                print "%s entered the room %s@%s." % \
+                    (sender.resource, 
+                     sender.node, 
+                     sender.domain)
+                in_room[sender.resource] = True
+            elif type == 'unavailable' and sender.resource in in_room:
+                print "%s left the room %s@%s" % (sender.resource, 
+                                                  sender.node, 
+                                                  sender.domain)
+                in_room.pop(sender.resource)
             for body in message.children("body"):
-                text = "<%s@%s/%s> %s" % (sender.node, sender.domain, sender.resource, body.text)
+                text = "%s %s" % (sendertxt, body.text)
                 print text
+
         if debug:
             print repr(elements.serialize())
 
@@ -42,12 +68,29 @@ def main(rooms, xmpp_jid, xmpp_password=None, log_file=None, debug=False):
     
         xmpp = yield inner.sub(connect(xmpp_jid, xmpp_password))
         xmpp.core.presence()
+        pipeline = None
         for room in rooms.split(","):
             room = room.strip()
             print 'Joining room %s.' % (room)
             watchroom = yield xmpp.muc.join(room)
-            inner.sub(watchroom | xmpp_to_stdout(xmpp_jid,debug) | threado.throws())
-        yield inner
+            print 'Participants:'
+            for participant in watchroom.participants:
+                print " %s" % participant.name.resource
+
+
+            if pipeline == None: 
+                pipeline = watchroom | xmpp_to_stdout(xmpp_jid,
+                                                      watchroom.participants,
+                                                      debug)
+            else:
+                pipeline |= watchroom | xmpp_to_stdout(xmpp_jid,
+                                                       watchroom.participants,
+                                                       debug)
+
+        # finally add threado.throws() that will pick up exceptions
+        pipeline |= threado.throws()
+        
+        yield pipeline
     return bot()
 
 main.rooms_help = "comma separated list of XMPP rooms roomreader should watch. " + \
