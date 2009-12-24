8acf7959e0393c0507f95e04e39ed530b6f9e021
jani <janike@iki.fi>
2009-12-24 13:14:25 +0000

    Temporary commit to share changes with multiple testing locations. Fixes propably follow.

--- openbsd/developer-create-package.sh
+++ openbsd/developer-create-package.sh
@@ -10,6 +10,21 @@ export PKG_PATH
 AH=abusehelper
 AHPORTS=${PORTSDIR}/${AH}
 
+revision=$(svn info|grep Revision|cut -d" " -f2)
+version=1.r${revision}
+
+mydir=$(dirname $0)
+
+makefile=${mydir}/abusehelper/Makefile 
+
+[ -f ${makefile}.in ] || myerr "Can not find ${makefile}.in"
+
+cat ${makefile}.in | sed -e "s/REPLACEVERSIONNUMBER/${version}/g" > ${makefile} 
+
+
+pkg_delete abusehelper
+echo 'Running userdel & groupdel'
+userdel _abusehe; groupdel _abusehe
 rm -fr ${AHPORTS}
 cp -r ${AH} ${PORTSDIR}/
 cd ${AHPORTS}
@@ -18,9 +33,7 @@ make makesum || myerr "Makesum failed."
 make checksum || myerr "Checksum failed."
 make plist || myerr "Creating plist failed."
 make package || myerr "Creating package failed."
-pkg_add abusehelper-r243
-pkg_delete abusehelper-r243
-echo 'Running userdel & groupdel'
-userdel _abusehe; groupdel _abusehe
+pkg_add abusehelper-${version}
+
 
 
