a4f0afcbbfe663fd404094c931edd18e8902d59d
Joachim Viide <jviide@iki.fi>
2010-02-23 17:21:37 +0000

    Fixes to imapbot logic.

--- abusehelper/core/imapbot.py
+++ abusehelper/core/imapbot.py
@@ -5,29 +5,40 @@ import email.parser
 
 from cStringIO import StringIO
 from abusehelper.core import events, bot, services
-from idiokit import threado, timer, util
+from idiokit import threado, timer
+
+def get_header(mailbox, num, section):
+    body_rex_str = r"\s*%s\s+\(BODY\[%s\]\s+" % (num, section)
+    body_rex = re.compile(body_rex_str, re.I)
+    
+    fetch = "(BODY.PEEK[%s])" % section
+    result, data = mailbox.fetch(num, fetch)
+
+    # Filter away parts that don't closely enough resemble tuple
+    # ("<MSGNUM> (BODY[<MSGPATH>.MIME] {<SIZE>}", "<MIMEHEADERS>")
+    data = [x for x in data if isinstance(x, tuple) and len(x) >= 2]
+    data = [x[1] for x in data if body_rex.match(x[0])]
+    if not data:
+        return None
+
+    return email.parser.Parser().parsestr(data[0], headersonly=True)
 
 def walk_mail(mailbox, num, path=(), headers=[]):
-    path = list(path) + [0]
+    if not path:
+        header = get_header(mailbox, num, "HEADER")
+        if header is None:
+            return
+        headers = headers + [header]
 
+    path = list(path) + [0]
     while True:
         path[-1] += 1
-        path_str = ".".join(map(str, path))
+        path_str = ".".join(map(str, path))        
 
-        body_rex_str = r"\s*%s\s+\(BODY\[%s.MIME\]\s+" % (num, path_str)
-        body_rex = re.compile(body_rex_str, re.I)
-
-        fetch = "(BODY.PEEK[%s.MIME])" % path_str
-        result, data = mailbox.fetch(num, fetch)
-
-        # Filter away parts that don't closely enough resemble tuple
-        # ("<MSGNUM> (BODY[<MSGPATH>.MIME] {<SIZE>}", "<MIMEHEADERS>")
-        data = [x for x in data if isinstance(x, tuple) and len(x) >= 2]
-        data = [x[1] for x in data if body_rex.match(x[0])]
-        if not data:
+        header = get_header(mailbox, num, path_str + ".MIME")
+        if header is None:
             return
 
-        header = email.parser.Parser().parsestr(data[0], headersonly=True)
         if header.get_content_maintype() == "multipart":
             for result in walk_mail(mailbox, num, path, headers + [header]):
                 yield result
@@ -84,6 +95,21 @@ class IMAPBot(bot.FeedBot):
         finally:
             yield inner.thread(mailbox.logout)
 
+    def _fetch(self, mailbox, num, path):
+        @threado.stream
+        def fetch(inner):
+            list(inner)
+            fetch = "(BODY.PEEK[%s])" % path
+            result, data = yield inner.thread(mailbox.fetch, num, fetch)
+            list(inner)
+            
+            for parts in data:
+                if not isinstance(parts, tuple) or len(parts) != 2:
+                    continue
+                reader = StringIO(parts[1])
+                inner.finish(StringIO(parts[1]))
+        return fetch
+
     @threado.stream
     def fetch_content(inner, self, mailbox, filter):
         yield inner.thread(mailbox.noop)
@@ -93,33 +119,38 @@ class IMAPBot(bot.FeedBot):
             return
 
         for num in data[0].split():
+            parts = list()
             for path, headers in walk_mail(mailbox, num):
-                main = headers[-1].get_content_maintype().replace("-", "__")
-                sub = headers[-1].get_content_subtype().replace("-", "__")
-        
-                handler_name = "handle_" + main + "_" + sub
-                handler = getattr(self, handler_name, self.handle_default)
-                if handler is None:
-                    continue
+                parts.append((headers, self._fetch(mailbox, num, path)))
 
-                fetch = "(BODY.PEEK[%s])" % path
-                result, data = yield inner.thread(mailbox.fetch, num, fetch)
-                list(inner)
-
-                skip_rest = False
-                for parts in data:
-                    if not isinstance(parts, tuple) or len(parts) != 2:
-                        continue
-                    reader = StringIO(parts[1])
-                    skip_rest = yield inner.sub(handler(headers, reader))
-                    list(inner)
-                    break
-                if skip_rest:
-                    break
-
+            if parts:
+                headers, _ = parts[0]
+                top_header = headers[0]
+                subject = top_header["Subject"] or "<no subject>"
+                sender = top_header["From"] or "<unknown sender>"
+                self.log.info("Handling mail %r from %r", subject, sender)
+                yield inner.sub(self.handle(parts))
             mailbox.store(num, "+FLAGS", "\\Seen")
 
-    handle_default = None
+    @threado.stream
+    def handle(inner, self, parts):
+        handle_default = getattr(self, "handle_default", None)
+
+        for headers, fetch in parts:
+            content_type = headers[-1].get_content_type()
+            suffix = content_type.replace("-", "__").replace("/", "_")
+
+            handler = getattr(self, "handle_" + suffix, handle_default)
+            if handler is None:
+                continue
+
+            list(inner)
+            fileobj = yield inner.sub(fetch())
+            list(inner)
+            skip_rest = yield inner.sub(handler(headers, fileobj))
+            if skip_rest:
+                return
+        list(inner)
 
 import base64
 import zipfile
@@ -130,7 +161,6 @@ class IMAPService(IMAPBot):
     filter = bot.Param(default=r'(BODY "http://" UNSEEN)')
     url_rex = bot.Param(default=r"http://\S+")
     filename_rex = bot.Param(default=r"(?P<eventfile>.*)")
-    from_url = bot.BoolParam()
 
     @threado.stream
     def normalize(inner, self, groupdict):
@@ -158,38 +188,48 @@ class IMAPService(IMAPBot):
         yield inner.sub(utils.csv_to_events(fileobj)
                         | self.normalize(match.groupdict()))
         inner.finish(True)
+        
+    def handle(self, parts):
+        attachments = list()
+        texts = list()
+
+        for headers, data in parts:
+            content_type = headers[-1].get_content_type()
+            if headers[-1].get_filename(None) is None:
+                if content_type == "text/plain":
+                    texts.append((headers, data))
+            elif content_type in ["text/plain", "application/zip"]:
+                attachments.append((headers, data))
+
+        return IMAPBot.handle(self, attachments + texts)
 
     @threado.stream
     def handle_text_plain(inner, self, headers, fileobj):
-        if self.from_url:
-            for match in re.findall(self.url_rex, fileobj.read()):
-                self.log.info("Fetching URL %r", match)
-                try:
-                    info, fileobj = yield inner.sub(utils.fetch_url(match))
-                except utils.FetchUrlFailed, fail:
-                    self.log.error("Fetching URL %r failed: %s", match, fail)
-                    return
+        filename = headers[-1].get_filename(None)
+        if filename is not None:
+            self.log.info("Parsing CSV data from an attachment")
+            result = yield inner.sub(self.parse_csv(filename, fileobj))
+            inner.finish(result)
 
-                filename = info.get_filename(None)
-                if filename is None:
-                    self.log.error("No filename given for the data")
-                    inner.finish(False)
+        for match in re.findall(self.url_rex, fileobj.read()):
+            self.log.info("Fetching URL %r", match)
+            try:
+                info, fileobj = yield inner.sub(utils.fetch_url(match))
+            except utils.FetchUrlFailed, fail:
+                self.log.error("Fetching URL %r failed: %r", match, fail)
+                return
 
-                self.log.info("Parsing CSV data from the URL")
-                skip_rest = yield inner.sub(self.parse_csv(filename, fileobj))
-                inner.finish(skip_rest)
-        else:
-            filename = headers[-1].get_filename(None)
-            if filename is not None:
-                self.log.info("Parsing CSV data from an attachment")
-                skip_rest = yield inner.sub(self.parse_csv(filename, fileobj))
-                inner.finish(skip_rest)                
+            filename = info.get_filename(None)
+            if filename is None:
+                self.log.error("No filename given for the data")
+                continue
+            
+            self.log.info("Parsing CSV data from the URL")
+            result = yield inner.sub(self.parse_csv(filename, fileobj))
+            inner.finish(result)
 
     @threado.stream
     def handle_application_zip(inner, self, headers, fileobj):
-        if self.from_url:
-            return
-
         self.log.info("Opening a ZIP attachment")
         try:
             data = base64.b64decode(fileobj.read())
@@ -208,9 +248,8 @@ class IMAPService(IMAPBot):
             csv_data = StringIO(zip.read(filename))
             
             self.log.info("Parsing CSV data from the ZIP attachment")
-            skip_rest = yield inner.sub(self.parse_csv(filename, csv_data))
-            if skip_rest:
-                inner.finish(skip_rest)
+            result = yield inner.sub(self.parse_csv(filename, csv_data))
+            inner.finish(result)
 
 if __name__ == "__main__":
     IMAPService.from_command_line().execute()
