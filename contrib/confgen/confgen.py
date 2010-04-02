ed7a58c84a5115ef4ab0913323594e9eb9b11362
jani <janike@iki.fi>
2010-04-02 15:31:58 +0000

    Reverted confgen as r519 broke python 2.5 compatibility.

--- contrib/confgen/confgen.py
+++ contrib/confgen/confgen.py
@@ -12,7 +12,6 @@ def dummy(_repr):
     return _dummy()
 
 NOT_GIVEN = dummy("<not given>")
-IGNORE_FILES = (".svn")
 
 def input(name, default=NOT_GIVEN, parser=NOT_GIVEN):
     prompt = name
@@ -62,7 +61,7 @@ src = os.path.join(dirname, "config-template")
 dst = sys.argv[1]
 
 try:
-    shutil.copytree(src, dst, ignore=shutil.ignore_patterns(IGNORE_FILES))
+    shutil.copytree(src, dst)
     try:
         replaces = dict(XMPP_JID=input("XMPP JID", parser=parse_jid),
                         XMPP_PASSWORD=input("XMPP password"),
