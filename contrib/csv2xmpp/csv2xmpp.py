83ccb33fa163b93b742f9f5da24f0f6168d7956d
jani <janike@iki.fi>
2010-04-11 10:33:35 +0000

    Historical event support using delay element. New option: --timestamp, for defining the timestamp row in csv.

--- contrib/csv2xmpp/csv2xmpp.py
+++ contrib/csv2xmpp/csv2xmpp.py
@@ -2,11 +2,16 @@ import csv
 import urllib2
 from idiokit import threado
 from abusehelper.core import events, bot, utils
+import time
+from idiokit import threado, util
+from idiokit.xmpp import Element
+
 
 class CSV2XMPP(bot.XMPPBot):
     csv_url = bot.Param()
     csv_delimiter = bot.Param(default=",")
     csv_columns = bot.ListParam(default=None)
+    timestamp_column = bot.Param(default=None)
     xmpp_room = bot.Param()
 
     @threado.stream
@@ -20,9 +25,50 @@ class CSV2XMPP(bot.XMPPBot):
         self.log.info("Joining room %r", self.xmpp_room)
         room = yield xmpp.muc.join(self.xmpp_room, self.bot_name)
 
-        yield inner.sub(utils.csv_to_events(fileobj, self.csv_delimiter, self.csv_columns)
-                        | events.events_to_elements() 
-                        | room)
+        if self.timestamp_column == None:
+            yield inner.sub(utils.csv_to_events(fileobj, self.csv_delimiter, self.csv_columns)
+                            | events.events_to_elements() 
+                            | room)
+        else:
+            yield inner.sub(utils.csv_to_events(fileobj, self.csv_delimiter, self.csv_columns)
+                            | events_to_elements_with_delay_element(self.timestamp_column) |room)
+
+@threado.stream_fast
+def events_to_elements_with_delay_element(inner,timestamp_column):
+    eventnum=0
+    while True:
+        eventnum += 1
+        yield inner
+        for event in inner:
+            print "Event %d" % (eventnum)
+            fields = list()
+            for key, values in event.attrs.iteritems():
+                for value in values:
+                    fields.append(key + "=" + value)
+            body = Element("body")
+            body.text = ", ".join(fields)
+            if event.contains(timestamp_column):
+                etime = event.value(timestamp_column)
+                inner.send(body,event.to_element(),delay_element(etime))
+            else:
+                inner.send(body,event.to_element())
+
+def delay_element(strstamp):
+    timestamp = time.mktime(time.strptime(strstamp,"%Y-%m-%dT%H:%M:%SZ"))
+    if time.daylight:
+        timestamp = timestamp + time.altzone
+    else:
+        timestamp = timestamp + time.timezone
+
+    delay = Element("delay")
+    delay.text = "Greetings earthlings"
+    delay.set_attr("xmlns", 'urn:xmpp:delay')
+    delay.set_attr("stamp", strstamp)
+    return delay
+
+def format_time(timestamp, format="%Y-%m-%d %H:%M:%S"):
+    return time.strftime(format, time.localtime(timestamp))
+
 
 if __name__ == "__main__":
     CSV2XMPP.from_command_line().run()
