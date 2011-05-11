e424f33a745b5eeefa6b829d5953cc91d84e4082
Joachim Viide <jviide@iki.fi>
2011-05-12 00:40:35 +0300

    contrib/experts/passivedns.py: Don't use the whois tool for the whois query.

--- contrib/experts/passivedns.py
+++ contrib/experts/passivedns.py
@@ -1,45 +1,53 @@
-import os
 import errno
-import signal
-import subprocess
-
-from idiokit import threado
+from idiokit import threado, sockets
 from abusehelper.core import bot, events
 from combiner import Expert
 
+DEFAULT_KEYS = ("domain", "ip", "first seen", "last seen")
+
 @threado.stream
-def lookup(inner, host, eid, name, keys=["domain", "ip", "first seen", "last seen"]):
-    process = subprocess.Popen(["whois", "-h", host, name],
-                               stdin=subprocess.PIPE,
-                               stdout=subprocess.PIPE,
-                               stderr=subprocess.PIPE)
+def collect(inner):
+    result = list()
+    while True:
+        try:
+            data = yield inner
+        except sockets.error, error:
+            if error.errno == errno.ECONNRESET:
+                break
+            raise
 
+        result.append(data)
+    inner.finish("".join(result))
+
+@threado.stream
+def lookup(inner, host, port, eid, name, keys=DEFAULT_KEYS):
+    sock = sockets.Socket()
     try:
-        stdout, stderr = yield inner.thread(process.communicate)
-    finally:
-        if process.poll() is None:
-            try:
-                os.kill(process.pid, signal.SIGKILL)
-            except OSError, ose:
-                if ose.errno != errno.ESRCH:
-                    raise
+        yield inner.sub(sock.connect((host, port)))
+        sock.send(name + "\r\n")
+        data = yield inner.sub(sock | collect())
+    except sockets.error, error:
+        return
 
-    for line in stdout.splitlines():
+    for line in data.splitlines():
         event = events.Event()
         for key, value in zip(keys, line.split("\t")):
             event.add(key, value)
-        inner.send(eid, event)
+        yield inner.send(eid, event)
 
 class PassiveDNSExpert(Expert):
     host = bot.Param()
+    port = bot.IntParam(default=43)
 
     @threado.stream
     def augment(inner, self):
         while True:
             eid, event = yield inner
             
-            for name in event.values("domain") + event.values("ip") + event.values("soa"):
-                yield inner.sub(lookup(self.host, eid, name))
+            for name in set(event.values("domain") + 
+                            event.values("ip") + 
+                            event.values("soa")):
+                yield inner.sub(lookup(self.host, self.port, eid, name))
 
 if __name__ == "__main__":
     PassiveDNSExpert.from_command_line().execute()
