6d054e2efcafe88e0bdfc843e5facfa253c35196
Joachim Viide <jviide@iki.fi>
2011-06-13 12:36:57 +0300

    contrib/runtime2dot: Make runtime2dot support mixed startup + runtime configs.

--- contrib/runtime2dot/runtime2dot.py
+++ contrib/runtime2dot/runtime2dot.py
@@ -1,31 +1,61 @@
-from abusehelper.core import bot
-from abusehelper.core.config import *
+from abusehelper.core import bot, config, startup, runtime
+
+def iter_dots(iterable):
+    for obj in iterable:
+        dot = getattr(obj, "__dot__", None)
+        if callable(dot):
+            yield dot()
+
+class Dot(object):
+    def __init__(self, *configs):
+        self.configs = tuple(config.flatten(configs))
+
+    def __dot__(self):
+        return self
+
+    def services(self):
+        services = set()
+        for dot in iter_dots(self.configs):
+            services.update(dot.services())
+
+        for conf in startup.iter_startups(self.configs):
+            service = dict(startup.params).get("bot_name", None)
+            services.add(service)
+
+        services.discard(None)
+        return services
+
+    def sessions(self):
+        sessions = set()
+        for dot in iter_dots(self.configs):
+            sessions.update(dot.sessions())
+
+        for session in runtime.iter_runtimes(self.configs):
+            sessions.add(session)
+
+        return sessions
 
 class DotBot(bot.Bot):
     bot_name = None
-    ini_file = None
-    ini_section = None
 
-    runtime_config = bot.Param("runtime configuration module")
-    startup_config = bot.Param("startup configuration module",
-                               default=None)
+    config = bot.Param("configuration module")
+    show_startups = bot.BoolParam()
     show_attributes = bot.BoolParam()
 
     def run(self):
         print "digraph G {"
         print "node [ shape=box, style=filled, color=lightgrey ];"
 
-        if self.startup_config is not None:
-            services = set()
-            for startup in load_configs(self.startup_config):
-                attrs = dict(startup.params)
-                services.add(attrs.get("bot_name", None))
-            services.discard(None)
+        dot = Dot(config.load_configs(self.config))
+        services = dot.services()
+        sessions = dot.sessions()
+
+        if self.show_startups:
             missing = lambda x: x not in services
         else:
             missing = lambda x: False
 
-        for session in load_configs(self.runtime_config):
+        for session in sessions:
             conf = dict(session.conf)
             path = session.path
             src = conf.pop("src_room", None)
