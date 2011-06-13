5024af88830754aab49d100249a1bd7d439e9d40
Joachim Viide <jviide@iki.fi>
2011-06-13 14:49:47 +0300

    contrib/abusechbot: Cleanups. Check for incoming exceptions.

--- contrib/abusechbot/abusechbot.py
+++ contrib/abusechbot/abusechbot.py
@@ -1,88 +1,77 @@
-import urllib2
 import xml.etree.cElementTree as etree
 from idiokit import threado
 from abusehelper.core import bot, events, utils
 
 class RSSBot(bot.PollingBot):
-    feeds = bot.ListParam("Urls to the RSS feeds.")
+    feeds = bot.ListParam("a list of RSS feed URLs")
+
+    def feed_keys(self, **_):
+        for feed in self.feeds:
+            yield (feed,)
 
     @threado.stream
-    def poll(inner, self, _):
-        yield
+    def poll(inner, self, url):
+        try:
+            self.log.info('Downloading feed from: "%s"', url)
+            _, fileobj = yield inner.sub(utils.fetch_url(url))
+        except utils.FetchUrlFailed, e:
+            self.log.error('Failed to download feed "%s": %r', url, e)
+            return
 
-        for url in self.feeds:
-            yield
-            try:
-                self.log.info('Downloading feed from: "%s"', url)
-                _, fileobj = yield inner.sub(utils.fetch_url(url))
-            except utils.FetchUrlFailed, e:
-                self.log.error('Failed to download feed "%s": %r', url, e)
-                return
+        for _, elem in etree.iterparse(fileobj):
+            items = elem.findall("item")
+            if not items:
+                continue
 
-            for _, elem in etree.iterparse(fileobj):
-                items = elem.findall("item")
-                if not items:
-                    continue
+            for item in items:
+                yield
+                for _ in inner: pass
 
-                for item in items:
-                    yield
-                    title = item.find("title")
-                    if title is not None:
-                        title = title.text
+                keys = ["title", "link", "description", "pubdate"]
+                args = [item.findtext(key) for key in keys]
+                args.append(url)
 
-                    link = item.find("link")
-                    if link is not None:
-                        link = link.text
+                event = self.create_event(*args)
+                if event:
+                    inner.send(event)
 
-                    description = item.find("description")
-                    if description is not None:
-                        description = description.text
+    def create_event(self, title, link, description, pubdate, url):
+        keys = dict(title=title, link=link, description=description, 
+                    pubdate=pubdate, url=url)
 
-                    pubdate = item.find("pubdate")
-                    if pubdate is not None:
-                        pubdate = pubdate.text
-
-                    event = self.create_event(title,link,description,pubdate,url)
-                    if event:
-                        inner.send(event)
-
-    def create_event(self, title, link, description, pubdate, url=None):
         event = events.Event()
-        if title:
-            event.add("title", title)
-        if link:
-            event.add("link", link)
-        if description:
-            event.add("description", description)
-        if pubdate:
-            event.add("pubdate", pubdate)
-        if url:
-            event.add("source", url)
+        for key, value in keys.iteritems():
+            if value:
+                event.add(key, value)
         return event
 
 class AbuseCHBot(RSSBot):
-    feeds = bot.ListParam(default=["https://spyeyetracker.abuse.ch/monitor.php?rssfeed=tracker", "https://zeustracker.abuse.ch/rss.php"])
+    feeds = bot.ListParam(
+        default=["https://spyeyetracker.abuse.ch/monitor.php?rssfeed=tracker", 
+                 "https://zeustracker.abuse.ch/rss.php"])
 
-    def create_event(self, title, link, description, pubdate, url=None):
+    def create_event(self, title, link, description, pubdate, url):
         if description is None:
-            return
+            return None
 
         event = events.Event()
         event.add("feed", "abuse.ch")
         if url:
             event.add("source",url)
+
         for part in description.split(","):
-            pair = part.split(":")
+            pair = part.split(":", 1)
             if len(pair) < 2:
                 continue
+
             key = pair[0].strip()
             value = pair[1].strip()
-
             if key == "Status" and value != "online":
-                return
-            elif not value:
+                return None
+            if not value:
                 continue
-            elif key == "AS":
+
+            if key == "AS":
                 if value.startswith("AS"):
                     value = value[2:]
                 event.add("as", value)
@@ -94,10 +83,8 @@ class AbuseCHBot(RSSBot):
                 event.add("host", value)
 
         if not event.contains("as") or not event.contains("ip"):
-            return
-
+            return None
         return event
 
 if __name__ == "__main__":
     AbuseCHBot.from_command_line().run()
-
