802115938d7b8d0b0fd5134c3dce39cab7d93e00
jani <janike@iki.fi>
2011-09-24 22:22:57 +0300

    opencollabreader: timer.sleep to avoid sending events before joining room. Stripping of [[ and ]] in values.

--- contrib/mdlbot/mdlbot2.py
+++ contrib/mdlbot/mdlbot2.py
@@ -1,6 +1,6 @@
 from idiokit import threado, util
 from abusehelper.core import utils, cymru, bot, events
-
+import urllib2,csv
 class MDLBot2(bot.PollingBot):
 	use_cymru_whois = bot.BoolParam(default=False)
 	
@@ -24,29 +24,45 @@ class MDLBot2(bot.PollingBot):
 			i += 1
     
 	@threado.stream
-	def poll(inner, self, url="http://www.malwaredomainlist.com/mdlcsv.php?inactive=off"):
-		
+	def poll(inner, self, url="http://www.malwaredomainlist.com/mdlcsv.php"):
+#		url = "http://www.malwaredomainlist.com/hostslist/ip.txt"
+		url = "http://www.malwaredomainlist.com/updatescsv.php"
 		self.log.info("Downloading MDL active sites list")
-		try:
-			info, fileobj = yield inner.sub(utils.fetch_url(url))
-			print info, fileobj
-		except utils.FetchUrlFailed, fuf:
-			self.log.error("MDL active sites downloading failed: %r", fuf)
-			return
+		fileobj = urllib2.urlopen("http://www.malwaredomainlist.com/mdlcsv.php").readlines()
+#		try:
+#			info, fileobj = yield inner.sub(utils.fetch_url(url))
+#			print info, fileobj.read()
+#		except utils.FetchUrlFailed, fuf:
+#			self.log.error("MDL active sites downloading failed: %r", fuf)
+#			return
 		self.log.info("MDL IP-list downloaded")
 		
-		charset = info.get_param("charset")
+#		charset = info.get_param("charset")
 		
-		if charset is None:
-			decode = util.guess_encoding
-		else:
-			decode = lambda x: x.decode(charset)
+#		if charset is None:
+#			decode = util.guess_encoding
+#		else:
+#			decode = lambda x: x.decode(charset)
 		
 		# date,domain,ip,reverse,description,registrant,asn,inactive,country
 		
 		columns = ["date", "url", "ip", "reverse", "description", "registrant", "asn", "inactive", "cc"]
-		
-		csv_to_events(inner, fileobj, ",", columns, charset)
+
+
+		lines = csv.DictReader(fileobj,columns)
+		charset = 'utf-8'
+		for line in lines:
+			event = events.Event()
+			for k,v in line.iteritems():
+				try:
+					event.add(k.decode(charset),v.decode(charset))
+				except UnicodeDecodeError,e:
+					self.log.info('Can not decode %s=%s' % (k,v))
+					continue
+			inner.send(event)
+			print event
+		inner.finish()
+
 
 if __name__ == "__main__":
 	MDLBot2.from_command_line().execute()
