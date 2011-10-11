8010c2ee68da376cdbba6f4b316bf1db9795e9ee
Joachim Viide <jviide@iki.fi>
2011-10-11 15:49:54 +0300

    contrib/confgen: Convert to use the new idiokit.
    --HG--
    branch : idiokit-3000

--- contrib/confgen/config-template/custom/sanitizer.py
+++ contrib/confgen/config-template/custom/sanitizer.py
@@ -1,6 +1,6 @@
 import socket
 import time as _time
-from idiokit import threado
+import idiokit
 from abusehelper.core import events, bot, taskfarm, services
 
 def format_time(time_tuple=None):
@@ -34,23 +34,23 @@ class Sanitizer(bot.ServiceBot):
         self.rooms = taskfarm.TaskFarm(self.handle_room)
         self.srcs = taskfarm.Counter()
 
-    @threado.stream
-    def handle_room(inner, self, name):
+    @idiokit.stream
+    def handle_room(self, name):
         self.log.info("Joining room %r", name)
-        room = yield inner.sub(self.xmpp.muc.join(name, self.bot_name))
+        room = yield self.xmpp.muc.join(name, self.bot_name)
         self.log.info("Joined room %r", name)
         try:
-            yield inner.sub(events.events_to_elements()
-                            | room
-                            | events.stanzas_to_events()
-                            | self.distribute(name))
+            yield idiokit.pipe(events.events_to_elements(),
+                               room,
+                               events.stanzas_to_events(),
+                               self.distribute(name))
         finally:
             self.log.info("Left room %r", name)
 
-    @threado.stream
-    def distribute(inner, self, name):
+    @idiokit.stream
+    def distribute(self, name):
         while True:
-            event = yield inner
+            event = yield idiokit.next()
 
             rooms = set(map(self.rooms.get, self.srcs.get(name)))
             rooms.discard(None)
@@ -59,15 +59,15 @@ class Sanitizer(bot.ServiceBot):
 
             for sanitized_event in self.sanitize(event):
                 for room in rooms:
-                    room.send(sanitized_event)
+                    yield room.send(sanitized_event)
 
-    @threado.stream
-    def session(inner, self, _, src_room, dst_room, **keys):
+    @idiokit.stream
+    def session(self, _, src_room, dst_room, **keys):
         self.srcs.inc(src_room, dst_room)
         try:
-            yield inner.sub(self.rooms.inc(src_room) | self.rooms.inc(dst_room))
+            yield self.rooms.inc(src_room) | self.rooms.inc(dst_room)
         except services.Stop:
-            inner.finish()
+            idiokit.stop()
         finally:
             self.srcs.dec(src_room, dst_room)
 
