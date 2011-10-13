1c15278dd1c100b7d17e844542752bcadb13c5ab
Joachim Viide <jviide@iki.fi>
2011-10-13 14:42:27 +0300

    contrib/urllistmailbot: Use threadpool.thread.
    --HG--
    branch : idiokit-3000

--- contrib/urllistmailbot/urllistmailbot.py
+++ contrib/urllistmailbot/urllistmailbot.py
@@ -20,6 +20,7 @@ import socket
 import urlparse
 
 import idiokit
+from idiokit import threadpool
 from abusehelper.core import imapbot, events, bot, cymru
 
 def get_hosts(url_lines):
@@ -63,7 +64,7 @@ class URLListMailBot(imapbot.IMAPBot):
 
         for url, host in get_hosts(url_lines):
             try:
-                addrinfo = yield imapbot.thread(socket.getaddrinfo, host, None)
+                addrinfo = yield threadpool.thread(socket.getaddrinfo, host, None)
             except socket.error, error:
                 self.log.info("Could not resolve host %r: %r", host, error)
                 continue
