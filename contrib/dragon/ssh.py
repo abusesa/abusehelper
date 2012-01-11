0d7c8d4f9ce807e3caa5295e68409acb2f629781
Joachim Viide <jviide@iki.fi>
2012-01-11 21:10:24 +0200

    contrib/dragon/ssh.py: Code cleanup.
    --HG--
    branch : idiokit-3000

--- contrib/dragon/ssh.py
+++ contrib/dragon/ssh.py
@@ -1,29 +1,18 @@
 import idiokit
 from idiokit import util
-from abusehelper.core import utils, cymru, bot, events
+from abusehelper.core import utils, bot, events
 
 class DragonSshBot(bot.PollingBot):
-    COLUMNS = ["asn","as name", "ip", "time", "category"]
-    use_cymru_whois = bot.BoolParam(default=False)
-
-    def __init__(self, *args, **keys):
-        bot.PollingBot.__init__(self, *args, **keys)
-        self.whois = cymru.CymruWhoisAugmenter()
-
-    def poll(self, asn):
-        if self.use_cymru_whois:
-            return self._poll() | self.whois.augment()
-        return self._poll()
+    COLUMNS = ["asn", "as name", "ip", "time", "category"]
 
     @idiokit.stream
-    def _poll(self, url="http://dragonresearchgroup.org/insight/sshpwauth.txt"):
+    def poll(self, _, url="http://dragonresearchgroup.org/insight/sshpwauth.txt"):
         self.log.info("Downloading %s" % url)
         try:
             info, fileobj = yield utils.fetch_url(url)
         except utils.FetchUrlFailed, fuf:
             self.log.error("Download failed: %r", fuf)
             return
-
         self.log.info("Downloaded")
 
         charset = info.get_param("charset")
@@ -33,7 +22,6 @@ class DragonSshBot(bot.PollingBot):
             decode = lambda x: x.decode(charset)
 
         filtered = (x for x in fileobj if x.strip() and not x.startswith("#"))
-        rows = list()
         yield utils.csv_to_events(filtered,
                                   delimiter="|",
                                   columns=self.COLUMNS,
