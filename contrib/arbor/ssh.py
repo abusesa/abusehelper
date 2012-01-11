b45d5ec200d43bb9036841e13fefd95795301330
Joachim Viide <jviide@iki.fi>
2012-01-11 21:10:07 +0200

    contrib/arbor/ssh.py: Updated to idiokit-3000. Code cleanup.
    --HG--
    branch : idiokit-3000

--- contrib/arbor/ssh.py
+++ contrib/arbor/ssh.py
@@ -1,24 +1,18 @@
-from idiokit import threado,util
+import idiokit
+from idiokit import util
 from abusehelper.core import utils, cymru, bot, events
-import re
-class ArborSshBot(bot.PollingBot):
+
+class ArborSSHBot(bot.PollingBot):
     COLUMNS = ["ip", "count"]
 
-    use_cymru_whois = bot.BoolParam(default=False)
+    def poll(self, _):
+        return self._poll() | cymru.CymruWhois()
 
-    def __init__(self, *args, **keys):
-        bot.PollingBot.__init__(self, *args, **keys)
-        self.whois = cymru.CymruWhoisAugmenter()
-
-    def poll(self,_):
-        return self._poll() | self.whois.augment()
-
-    @threado.stream
-    def _poll(inner, self, url="http://atlas-public.ec2.arbor.net/public/ssh_attackers"):
-
+    @idiokit.stream
+    def _poll(self, url="http://atlas-public.ec2.arbor.net/public/ssh_attackers"):
         self.log.info("Downloading %s" % url)
         try:
-            info, fileobj = yield inner.sub(utils.fetch_url(url))
+            info, fileobj = yield utils.fetch_url(url)
         except utils.FetchUrlFailed, fuf:
             self.log.error("Download failed: %r", asn, fuf)
             return
@@ -30,17 +24,16 @@ class ArborSshBot(bot.PollingBot):
         else:
             decode = lambda x: x.decode(charset)
 
-        filtered = (x for x in fileobj if x.strip() and not x.startswith("other"))
-        rows = list()
-        #sanitize delimiter
-        for row in filtered:
-            row = decode(row.strip())
-            rows.append("\t".join(re.split("\s+",row)))
-        yield inner.sub(utils.csv_to_events(rows,
-                                            delimiter="\t",
-                                            columns=self.COLUMNS,
-                                            charset=charset))
+        for row in fileobj:
+            row = row.strip()
+            if row.startswith("other"):
+                continue
 
+            row = decode(row).split()
+            event = events.Event()
+            for key, value in zip(self.COLUMNS, row):
+                event.add(key, value)
+            yield idiokit.send(event)
 
 if __name__ == "__main__":
-    ArborSshBot.from_command_line().execute()
+    ArborSSHBot.from_command_line().execute()
