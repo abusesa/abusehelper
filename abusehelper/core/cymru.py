2d9304030e4a6900f7d51bdc2242458108bc44b5
Joachim Viide <jviide@iki.fi>
2012-01-11 18:33:22 +0200

    abusehelper.core.cymru: A different timing style for doing cymruwhois lookups.
    --HG--
    branch : idiokit-3000

--- abusehelper/core/cymru.py
+++ abusehelper/core/cymru.py
@@ -1,3 +1,4 @@
+import socket
 import idiokit
 from abusehelper.core import utils
 from idiokit import util, sockets, timer
@@ -5,152 +6,120 @@ from idiokit import util, sockets, timer
 class Stop(Exception):
     pass
 
-class CymruWhoisAugmenter(object):
-    LINE_KEYS = "asn", "bgp_prefix", "cc", "registry", "allocated", "as name"
+def is_ip(string):
+    for addr_type in (socket.AF_INET, socket.AF_INET6):
+        try:
+            socket.inet_pton(addr_type, string)
+        except (ValueError, socket.error):
+            pass
+        else:
+            return True
+    return False
 
-    def __init__(self, throttle_time=2.0, cache_time=60*60.0):
-        self.throttle_time = throttle_time
+class CymruWhoisAugmenter(object):
+    KEYS = "asn", "bgp_prefix", "cc", "registry", "allocated", "as name"
+
+    def __init__(self, wait_time=0.5, cache_time=60*60.0):
+        self.wait_time = wait_time
 
         self.cache = utils.TimedCache(cache_time)
+        self.socket = None
+        self.buffer = None
+        self.main = None
+        self.count = 0
 
-        self.global_main = None
-        self.global_channels = set()
-        self.global_pending = dict()
+    @idiokit.stream
+    def augment(self, ip_key="ip"):
+        while True:
+            event = yield idiokit.next()
 
-    def main(self, channels, pending):
-        main = self._main(channels, pending)
-        idiokit.pipe(self._alert(self.throttle_time), main)
-        return main
+            for ip in event.values(ip_key, filter=is_ip):
+                items = yield self.resolve(ip)
+                for key, value in items:
+                    event.add(key, value)
+
+            yield idiokit.send(event)
+
+    @idiokit.stream
+    def resolve(self, ip):
+        self.count += 1
+        try:
+            if self.main is None:
+                self.main = self._main()
+                idiokit.pipe(self._alert(self.wait_time / 2), self.main)
+            main = self.main
+
+            event = idiokit.Event()
+            yield main.send(ip, event)
+            values = yield event | main.fork() | event
+        finally:
+            self.count -= 1
+
+        idiokit.stop([x for x in zip(self.KEYS, values) if x[1] is not None])
 
     @idiokit.stream
     def _alert(self, interval):
-        yield timer.sleep(interval / 2.0)
-        yield idiokit.send()
-
         while True:
             yield timer.sleep(interval)
             yield idiokit.send()
 
     @idiokit.stream
-    def _main(self, channels, pending):
+    def _main(self):
+        timeouts = 0
         try:
-            while True:
-                yield idiokit.next()
-                yield self.iteration(pending)
-        except Stop:
-            pass
-        except:
-            for channel in channels:
-                channel.throw()
-            raise
+            while not (timeouts >= 2 and self.count <= 0):
+                item = yield idiokit.next()
+                if item is None:
+                    timeouts += 1
+                    if timeouts >= 2:
+                        yield self._close()
+                    continue
 
-        for channel in channels:
-            channel.throw(Stop())
-
-    @idiokit.stream
-    def _forward(self, global_main, local_pending):
-        should_stop = False
-
-        while not should_stop or local_pending:
-            try:
-                ip, values = yield idiokit.next()
-
-                events = local_pending.pop(ip, ())
-
-                for event in events:
-                    for key, value in zip(self.LINE_KEYS, values):
-                        if value is not None:
-                            event.add(key, value)
-                    yield idiokit.send(event)
-            except StopIteration:
-                should_stop = True
-                yield global_main.send()
-
-    @idiokit.stream
-    def _collect(self, key, channel, local_pending, global_pending):
-        while True:
-            event = yield idiokit.next()
-
-            for ip in event.values(key):
-                local_pending.setdefault(ip, list()).append(event)
-
+                ip, event = item
                 values = self.cache.get(ip, None)
+                if values is None:
+                    values = yield self._resolve(ip)
+                    self.cache.set(ip, tuple(values))
+                    timeouts = 0
+                event.succeed(values)
+        finally:
+            self.main = None
+
+    @idiokit.stream
+    def _resolve(self, ip):
+        if self.socket is None:
+            self.socket = sockets.Socket()
+            yield self.socket.connect(("whois.cymru.com", 43))
+            yield self.socket.writeall("begin\nverbose\n")
+
+            self.buffer = util.LineBuffer()
+
+        yield self.socket.writeall(ip + "\n")
+        while True:
+            data = yield self.socket.read(4096)
+
+            for line in self.buffer.feed(data):
+                values = self._parse(line)
                 if values is not None:
-                    yield idiokit.send(ip, values)
-                else:
-                    global_pending.setdefault(ip, set()).add(channel)
+                    idiokit.stop(values)
 
     @idiokit.stream
-    def augment(self, key="ip"):
-        if self.global_main is None:
-            self.global_main = self.main(self.global_channels,
-                                         self.global_pending)
+    def _close(self):
+        if self.socket is not None:
+            yield self.socket.writeall("end\n")
+            yield self.socket.close()
+        self.socket = None
+        self.buffer = None
 
-        global_channels = self.global_channels
-        global_pending = self.global_pending
-        global_main = self.global_main
+    def _parse(self, line):
+        line = line.decode("utf-8", "replace")
+        bites = [x.strip() for x in line.split("|")]
+        bites = [x if x not in ("", "NA") else None for x in bites]
+        if len(bites) != 7:
+            return None
 
-        local_pending = dict()
-        forward = self._forward(global_main, local_pending)
-        collect = self._collect(key, forward, local_pending, global_pending)
-
-        global_channels.add(forward)
-
-        try:
-            yield idiokit.pipe(collect, forward)
-        finally:
-            for ip in local_pending:
-                forwards = global_pending.get(ip, set())
-                forwards.discard(forward)
-                if not forwards:
-                    global_pending.pop(ip, None)
-
-            global_channels.discard(forward)
-            if not global_channels:
-                global_main.throw(Stop())
-                if self.global_main is global_main:
-                    self.global_main = None
-                yield global_main
-
-    @idiokit.stream
-    def iteration(self, pending):
-        if not pending:
-            return
-
-        socket = sockets.Socket()
-        try:
-            yield socket.connect(("whois.cymru.com", 43))
-
-            ips = set(pending)
-
-            yield socket.writeall("begin\n")
-            yield socket.writeall("verbose\n")
-            for ip in ips:
-                yield socket.writeall(ip + "\n")
-            yield socket.writeall("end\n")
-
-            line_buffer = util.LineBuffer()
-            while ips:
-                data = yield socket.read(4096)
-
-                for line in line_buffer.feed(data):
-                    line = line.decode("utf-8", "replace")
-                    bites = [x.strip() for x in line.split("|")]
-                    bites = [x if x not in ("", "NA") else None for x in bites]
-                    if len(bites) != 7:
-                        continue
-                    ip = bites.pop(1)
-
-                    ips.discard(ip)
-                    self.cache.set(ip, bites)
-
-                    channels = pending.pop(ip, ())
-                    for channel in channels:
-                        channel.send(ip, bites)
-        except sockets.error:
-            pass
-        finally:
-            yield socket.close()
+        bites.pop(1)
+        return bites
 
 global_whois = CymruWhoisAugmenter()
 
