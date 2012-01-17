b8c7587f6b0cbae8fe1c6c3de83ff15bd3c8821b
Joachim Viide <jviide@iki.fi>
2012-01-17 15:31:59 +0200

    contrib/danger/bruteforceblocker.py: Cleanup + poll(self, _) -> poll(self).
    --HG--
    branch : idiokit-3000

--- contrib/danger/bruteforceblocker.py
+++ contrib/danger/bruteforceblocker.py
@@ -1,36 +1,26 @@
-# -*- coding: utf-8 -*-
 """
-    Feed handler for bruteforceblocker list in danger.rulez.sk
+Feed handler for bruteforceblocker list in danger.rulez.sk.
+
+Maintainer: Jussi Eronen <exec@iki.fi>
 """
-__authors__ = "Jussi Eronen"
-__copyright__ = "Copyright 2011, The AbuseHelper Project"
-__license__ = "MIT <http://www.opensource.org/licenses/mit-license.php>"
-__maintainer__ = "Jussi Eronen"
-__email__ = "exec@iki.fi"
 
 import re
 import cStringIO
 
 import idiokit
-from idiokit import util
-from abusehelper.core import utils, cymru, bot, events
+from abusehelper.core import utils, cymru, bot
 
 class BruteForceBlockerBot(bot.PollingBot):
     COLUMNS = ["ip", "lastseen", "count", "id", "url"]
     use_cymru_whois = bot.BoolParam(default=True)
 
-    def __init__(self, *args, **keys):
-        bot.PollingBot.__init__(self, *args, **keys)
-        self.whois = cymru.CymruWhoisAugmenter()
-
-    def poll(self,_):
+    def poll(self):
         if self.use_cymru_whois:
-            return self._poll() | self.whois.augment()
+            return self._poll() | cymru.CymruWhois()
         return self._poll()
 
     @idiokit.stream
-    def _poll(self, 
-             url="http://danger.rulez.sk/projects/bruteforceblocker/blist.php"):
+    def _poll(self, url="http://danger.rulez.sk/projects/bruteforceblocker/blist.php"):
         self.log.info("Downloading %s" % url)
         try:
             info, fileobj = yield utils.fetch_url(url)
@@ -48,14 +38,7 @@ class BruteForceBlockerBot(bot.PollingBot):
         fileobj = cStringIO.StringIO(data)
 
         charset = info.get_param("charset")
-        if charset is None:
-            decode = util.guess_encoding
-        else:
-            decode = lambda x: x.decode(charset)
-
         filtered = (x for x in fileobj if x.strip() and not x.startswith("#"))
-        rows = list()
-
         yield utils.csv_to_events(filtered,
                                   delimiter="\t",
                                   columns=self.COLUMNS,
