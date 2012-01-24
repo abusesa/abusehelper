87001063528d83c9ddcf457cde3116d10b4b39a0
Joachim Viide <jviide@iki.fi>
2012-01-24 09:21:42 +0200

    contrib/experts/geoipexpert.py: Skip non-IPv4 values when deciding what to augment. Clean up code.
    --HG--
    branch : idiokit-3000

--- contrib/experts/geoipexpert.py
+++ contrib/experts/geoipexpert.py
@@ -1,8 +1,17 @@
+import socket
+
 import pygeoip
 
 import idiokit
 from abusehelper.core import events, bot
 from abusehelper.contrib.experts.combiner import Expert
+
+def is_ipv4(ip):
+    try:
+        socket.inet_aton(ip)
+    except (ValueError, socket.error):
+        return False
+    return True
 
 class GeoIPExpert(Expert):
     geoip_db = bot.Param("path to the GeoIP database")
@@ -13,40 +22,38 @@ class GeoIPExpert(Expert):
         Expert.__init__(self, *args, **keys)
 
         self.geoip = pygeoip.GeoIP(self.geoip_db)
-        self.log.info('GeoIP initiated')
+        self.log.info("GeoIP initiated")
 
     def geomap(self, event, key):
-        ip = event.value(key, None)
-        if not ip:
-            return
+        for ip in event.values(key, filter=is_ipv4):
+            try:
+                record = self.geoip.record_by_addr(ip)
+            except pygeoip.GeoIPError, e:
+                self.log.error("GeoIP fetch failed: %s", repr(e))
+                continue
 
-        try:
-            record = self.geoip.record_by_addr(ip)
-        except pygeoip.GeoIPError, e:
-            self.log.error("GeoIP fetch failed: %s", repr(e))
-            return
+            if not record:
+                continue
 
-        if not record:
-            return
+            augmentation = events.Event()
 
-        augmentation = events.Event()
-
-        if not event.contains('cc'):
-            cc = record.get('country_code', None)
+            cc = record.get("country_code", None)
             if cc:
-                augmentation.add('geoip_cc', cc)
+                augmentation.add("geoip_cc", cc)
 
-        if not event.contains('latitude'):
-            latitude = record.get('latitude', None)
+            latitude = record.get("latitude", None)
             if latitude:
-                augmentation.add('latitude', unicode(latitude))
+                augmentation.add("latitude", unicode(latitude))
 
-        if not event.contains('longitude'):
-            longitude = self.geoip.record_by_addr(ip)['longitude']
+            longitude = record.get("longitude", None)
             if longitude:
-                augmentation.add('longitude', unicode(longitude))
+                augmentation.add("longitude", unicode(longitude))
 
-        yield augmentation
+            if not augmentation.contains():
+                continue
+
+            augmentation.add(key, ip)
+            yield augmentation
 
     @idiokit.stream
     def augment(self):
