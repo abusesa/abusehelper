2e4b1fa9d67c9d5e691b67175af1d0aff2476802
Joachim Viide <jviide@iki.fi>
2012-02-01 15:02:03 +0200

    contrib/tailbot/tailbot.py: Separated tailbot.tail_file from tailbot.TailBot.
    --HG--
    branch : idiokit-3000

--- contrib/tailbot/tailbot.py
+++ contrib/tailbot/tailbot.py
@@ -1,6 +1,6 @@
 import os
-from abusehelper.core import events, bot
 import idiokit
+from abusehelper.core import events, bot
 
 def follow_file(filename):
     prev_inode = None
@@ -33,10 +33,10 @@ def follow_file(filename):
                 yield None
                 prev_inode = prev_size = prev_mtime = None
                 continue
-            yield mtime, opened
+            yield True, mtime, opened
         elif prev_size != size and prev_mtime != mtime:
             opened.seek(opened.tell())
-            yield mtime, opened
+            yield False, mtime, opened
         else:
             yield None
 
@@ -44,32 +44,73 @@ def follow_file(filename):
         prev_mtime = mtime
         prev_inode = inode
 
+def tail_file(filename, offset=None):
+    first = True
+    buffer = []
+
+    for result in follow_file(filename):
+        if first and result is not None:
+            _, _, opened = result
+
+            if offset is None:
+                opened.seek(0, os.SEEK_END)
+            elif offset >= 0:
+                opened.seek(offset)
+            else:
+                opened.seek(offset, os.SEEK_END)
+        first = False
+
+        if result is None:
+            yield None
+            continue
+
+        flush, mtime, opened = result
+        if flush and buffer:
+            buffer = []
+
+        while True:
+            data = opened.read(4096)
+            if not data:
+                break
+
+            lines = data.split("\n")
+            if len(lines) <= 1:
+                buffer.extend(lines)
+                continue
+
+            lines[0] = "".join(buffer) + lines[0]
+            for line in lines[:-1]:
+                if line.endswith("\r"):
+                    line = line[:-1]
+                yield mtime, line
+
+            if not lines[-1]:
+                buffer = []
+            else:
+                buffer = lines[-1:]
+
+        yield None
+
 class TailBot(bot.FeedBot):
     path = bot.Param("path to the followed file")
 
     @idiokit.stream
     def feed(self):
-        first = True
+        for result in tail_file(self.path):
+            if result is None:
+                yield idiokit.timer.sleep(2.0)
+                continue
 
-        for result in follow_file(self.path):
-            yield idiokit.timer.sleep(2.0)
+            mtime, line = result
 
-            if result is not None:
-                mtime, opened = result
-                if first:
-                    opened.seek(0, os.SEEK_END)
+            keys = self.parse(line, mtime)
+            if keys is None:
+                continue
 
-                for line in opened:
-                    keys = self.parse(line, mtime)
-                    if keys is None:
-                        continue
-
-                    event = events.Event()
-                    for key, value in keys.items():
-                        event.add(key, value)
-                    yield idiokit.send(event)
-
-            first = False
+            event = events.Event()
+            for key, value in keys.items():
+                event.add(key, value)
+            yield idiokit.send(event)
 
     def parse(self, line, mtime):
         line = line.rstrip()
