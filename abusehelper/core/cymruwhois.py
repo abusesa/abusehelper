df12bd260d5492ba8c6944bcd32c425d1327f727
exec <exec@iki.fi>
2012-02-29 12:19:19 +0200

    Added exception for TypeError to ip address test before doing cymru lookup for possibly malformed data (random unicode strings etc).

--- abusehelper/core/cymruwhois.py
+++ abusehelper/core/cymruwhois.py
@@ -7,7 +7,7 @@ def is_ip(string):
     for addr_type in (socket.AF_INET, socket.AF_INET6):
         try:
             socket.inet_pton(addr_type, string)
-        except (ValueError, socket.error):
+        except (TypeError, ValueError, socket.error):
             pass
         else:
             return True
