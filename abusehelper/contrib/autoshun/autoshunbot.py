2a67ef989fa79aefccb9bcefe543715346642f91
Louai Al-Khanji <louai@al-khanji.fi>
2012-06-25 10:38:25 +0300

    Normalize time to UTC.

--- abusehelper/contrib/autoshun/autoshunbot.py
+++ abusehelper/contrib/autoshun/autoshunbot.py
@@ -1,4 +1,6 @@
 import idiokit
+import time as _time
+import calendar
 from abusehelper.core import utils, cymruwhois, bot, events
 
 AUTOSHUN_CSV_URL = "http://www.autoshun.org/files/shunlist.csv"
@@ -40,7 +42,19 @@ class AutoshunBot(bot.PollingBot):
             event = yield idiokit.next()
             event.add("feed", "autoshun")
             event.add("source url", self.feed_url)
+
+            times = event.values("time")
+            event.clear("time")
+            for time in times:
+                event.add("time", self._normalize_time(time))
             yield idiokit.send(event)
+
+    def _normalize_time(self, time):
+        parsed = _time.strptime(time, "%Y-%m-%d %H:%M:%S")
+        seconds = calendar.timegm(parsed)
+        seconds += 5 * 3600 # UTC-5 to UTC
+        time_tuple = _time.gmtime(seconds)
+        return _time.strftime("%Y-%m-%d %H:%M:%S UTC", time_tuple)
 
 if __name__ == "__main__":
     AutoshunBot.from_command_line().execute()
