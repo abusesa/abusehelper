8adcbb8b1d647f2b012bb65c6ba7ec385a6f2c9a
Louai Al-Khanji <louai@al-khanji.fi>
2012-06-25 13:57:39 +0300

    Add dragon research group vncprobe bot.

---
+++ abusehelper/contrib/dragonresearchgroup/vnc.py
@@ -0,0 +1,29 @@
+import idiokit
+from abusehelper.core import utils, cymruwhois, bot
+
+class DragonVncBot(bot.PollingBot):
+    # The first column values (ASN and AS name) are ignored.
+    COLUMNS = [None, None, "ip", "time", "category"]
+
+    def poll(self):
+        return self._poll() | cymruwhois.augment("ip")
+
+    @idiokit.stream
+    def _poll(self, url="https://dragonresearchgroup.org/insight/vncprobe.txt"):
+        self.log.info("Downloading %s" % url)
+        try:
+            info, fileobj = yield utils.fetch_url(url)
+        except utils.FetchUrlFailed, fuf:
+            self.log.error("Download failed: %r", fuf)
+            return
+        self.log.info("Downloaded")
+
+        charset = info.get_param("charset")
+        filtered = (x for x in fileobj if x.strip() and not x.startswith("#"))
+        yield utils.csv_to_events(filtered,
+                                  delimiter="|",
+                                  columns=self.COLUMNS,
+                                  charset=charset)
+
+if __name__ == "__main__":
+    DragonVncBot.from_command_line().execute()
