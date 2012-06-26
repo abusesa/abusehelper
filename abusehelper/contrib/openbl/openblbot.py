f963fee9b4229776396815ff3b9ed233f95cdd05
Louai Al-Khanji <louai@al-khanji.fi>
2012-06-26 11:18:45 +0300

    Add OpenBL bot.

---
+++ abusehelper/contrib/openbl/openblbot.py
@@ -0,0 +1,53 @@
+import idiokit
+import time as _time
+
+from abusehelper.core import utils, cymruwhois, bot, events
+
+OPENBL_FEED_URL = "https://www.openbl.org/lists/date.txt"
+
+class OpenBLBot(bot.PollingBot):
+    feed_url = bot.Param(default=OPENBL_FEED_URL)
+    use_cymru_whois = bot.BoolParam(default=True)
+
+    def poll(self):
+        pipe = self._poll(url=self.feed_url)
+
+        if self.use_cymru_whois:
+            pipe = pipe | cymruwhois.augment("ip")
+
+        return pipe
+
+    @idiokit.stream
+    def _poll(self, url):
+        self.log.info("Downloading %s" % url)
+        try:
+            info, fileobj = yield utils.fetch_url(url)
+        except utils.FetchUrlFailed, fuf:
+            self.log.error("Download failed: %r", fuf)
+            idiokit.stop()
+        self.log.info("Downloaded")
+
+        for line in fileobj:
+            if line.startswith("#"):
+                continue
+
+            ip, time = line.split()
+            time = self._normalize_time(time)
+
+            event = events.Event()
+            event.add("ip", ip)
+            event.add("time", time)
+            event.add("feed", "OpenBL")
+            event.add("source url", self.feed_url)
+            event.add("type", "ssh brute-force")
+
+            yield idiokit.send(event)
+
+    def _normalize_time(self, time):
+        seconds = int(time)
+        seconds -= 1 * 3600 # UTC+1 to UTC
+        time_tuple = _time.gmtime(seconds)
+        return _time.strftime("%Y-%m-%d %H:%M:%S UTC", time_tuple)
+
+if __name__ == "__main__":
+    OpenBLBot.from_command_line().execute()
