f2d63d97c555b3098228b48989a58c5e7e53356f
Louai Al-Khanji <louai@al-khanji.fi>
2012-11-05 13:54:05 +0200

    Report events in blocklist order. Change type to spam.

--- abusehelper/contrib/spamhaus/sbl.py
+++ abusehelper/contrib/spamhaus/sbl.py
@@ -9,7 +9,7 @@ class SpamhausSblBot(bot.PollingBot):
     def _event(self, spamhaus_id):
         event = events.Event()
         event.add("feed", "spamhaus block list")
-        event.add("type", "spambot")
+        event.add("type", "spam")
         if spamhaus_id:
             spamhaus_id = "SBL" + str(int(spamhaus_id.replace("$", ""), base=10))
             event.add("spamhaus id", spamhaus_id)
@@ -17,35 +17,20 @@ class SpamhausSblBot(bot.PollingBot):
         return event
 
     @idiokit.stream
-    def _cymru_augment(self, event, key):
-        if self.use_cymru_whois:
-            values = yield cymruwhois.lookup(key)
-            for key, value in values:
-                event.add(key, value)
-        yield idiokit.stop(event)
-
-    @idiokit.stream
-    def _ip_events(self, ips):
-        for ip, spamhaus_id in ips:
-            event = self._event(spamhaus_id)
-            event.add("ip", ip)
-            event = yield self._cymru_augment(event, ip)
-            yield idiokit.send(event)
-
-    @idiokit.stream
-    def _netblock_events(self, netblocks):
-        for netblock, spamhaus_id in netblocks:
-            event = self._event(spamhaus_id)
-            event.add("netblock", netblock)
-            event = yield self._cymru_augment(event, netblock.split("/")[0])
-            yield idiokit.send(event)
+    def _cymru_augment(self, event):
+        if event.contains("ip"):
+            key = event.value("ip")
+        else:
+            key = event.value("netblock")
+        values = yield cymruwhois.lookup(key)
+        for key, value in values:
+            event.add(key, value)
 
     @idiokit.stream
     def poll(self):
         skip_chars = ["#", ":", "$"]
         self.log.info("Opening %s" % self.sbl_filepath)
-        ips = []
-        netblocks = []
+        entries = []
 
         try:
             with open(self.sbl_filepath, "r") as f:
@@ -56,20 +41,27 @@ class SpamhausSblBot(bot.PollingBot):
                     parts = line.split()
 
                     if len(parts) == 1:
-                        ips.append((parts[0], None))
+                        entries.append((parts[0], None))
                     elif len(parts) == 2:
-                        if parts[0].endswith("/32"):
-                            ip, _ = parts[0].split("/")
-                            ips.append((ip, parts[1]))
-                        else:
-                            netblocks.append((parts[0], parts[1]))
+                        entries.append((parts[0], parts[1]))
 
-            self.log.info("Read %d ip addresses, %d netblocks" % (len(ips), len(netblocks)))
+            self.log.info("Read %d entries" % len(entries))
         except IOError, ioe:
             self.log.error("Could not open %s: %s" % (self.sbl_filepath, ioe))
 
-        yield self._ip_events(ips)
-        yield self._netblock_events(netblocks)
+        for entry, case_id in entries:
+            event = self._event(case_id)
+            if "/" in entry:
+                prefix, suffix = entry.split("/")
+                if suffix == "32":
+                    event.add("ip", prefix)
+                else:
+                    event.add("netblock", entry)
+            else:
+                event.add("ip", entry)
+            if self.use_cymru_whois:
+                yield self._cymru_augment(event)
+            yield idiokit.send(event)
 
 if __name__ == "__main__":
     SpamhausSblBot.from_command_line().execute()
