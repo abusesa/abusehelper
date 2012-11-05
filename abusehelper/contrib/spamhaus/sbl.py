83f0259e08ef8beac379c48029e8206ce03c3331
Louai Al-Khanji <louai@al-khanji.fi>
2012-11-05 10:07:23 +0200

    Report spamhaus id and source url.

--- abusehelper/contrib/spamhaus/sbl.py
+++ abusehelper/contrib/spamhaus/sbl.py
@@ -6,10 +6,14 @@ class SpamhausSblBot(bot.PollingBot):
     use_cymru_whois = bot.BoolParam()
     sbl_filepath = bot.Param("Filename of Spamhaus SBL file")
 
-    def _event(self):
+    def _event(self, spamhaus_id):
         event = events.Event()
         event.add("feed", "spamhaus block list")
         event.add("type", "spambot")
+        if spamhaus_id:
+            spamhaus_id = "SBL" + str(int(spamhaus_id.replace("$", ""), base=10))
+            event.add("spamhaus id", spamhaus_id)
+            event.add("source url", "http://www.spamhaus.org/sbl/query/" + spamhaus_id)
         return event
 
     @idiokit.stream
@@ -22,16 +26,16 @@ class SpamhausSblBot(bot.PollingBot):
 
     @idiokit.stream
     def _ip_events(self, ips):
-        for ip in ips:
-            event = self._event()
+        for ip, spamhaus_id in ips:
+            event = self._event(spamhaus_id)
             event.add("ip", ip)
             event = yield self._cymru_augment(event, ip)
             yield idiokit.send(event)
 
     @idiokit.stream
     def _netblock_events(self, netblocks):
-        for netblock in netblocks:
-            event = self._event()
+        for netblock, spamhaus_id in netblocks:
+            event = self._event(spamhaus_id)
             event.add("netblock", netblock)
             event = yield self._cymru_augment(event, netblock.split("/")[0])
             yield idiokit.send(event)
@@ -52,13 +56,13 @@ class SpamhausSblBot(bot.PollingBot):
                     parts = line.split()
 
                     if len(parts) == 1:
-                        ips.append(parts[0])
+                        ips.append((parts[0], None))
                     elif len(parts) == 2:
                         if parts[0].endswith("/32"):
                             ip, _ = parts[0].split("/")
-                            ips.append(ip)
+                            ips.append((ip, parts[1]))
                         else:
-                            netblocks.append(parts[0])
+                            netblocks.append((parts[0], parts[1]))
 
             self.log.info("Read %d ip addresses, %d netblocks" % (len(ips), len(netblocks)))
         except IOError, ioe:
