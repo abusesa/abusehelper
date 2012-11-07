6010f6b4e3de736b37d0259dbb3cf41787186488
Lari Huttunen <github@huttu.net>
2012-11-07 14:35:18 +0100

    abusehelper.contrib.abusech: Removed cruft code and added zeusccbot to handle that specific feed.

--- abusehelper/contrib/abusech/zeusccbot.py
+++ abusehelper/contrib/abusech/zeusccbot.py
@@ -1,5 +1,5 @@
 """
-SpyEye C&C RSS feed bot.
+abuse.ch Zeus C&C RSS feed bot.
 
 Maintainer: Lari Huttunen <mit-code@huttu.net>
 """
@@ -9,8 +9,8 @@ from abusehelper.core import bot, events
 from abusehelper.contrib.rssbot.rssbot import RSSBot
 
 
-class SpyEyeCcBot(RSSBot):
-    feeds = bot.ListParam(default=["https://spyeyetracker.abuse.ch/monitor.php?rssfeed=tracker"])
+class ZeusCcBot(RSSBot):
+    feeds = bot.ListParam(default=["https://zeustracker.abuse.ch/rss.php"])
     # If treat_as_dns_source is set, the feed ip is dropped.
     treat_as_dns_source = bot.BoolParam()
 
@@ -66,9 +66,20 @@ class SpyEyeCcBot(RSSBot):
                 value = pair[1].strip()
                 if not key or not value:
                     continue
+        # handle description data
+        description = keys.get("description", None)
+        if description:
+            for part in description.split(","):
+                pair = part.split(":", 1)
+                if len(pair) < 2:
+                    continue
+                key = pair[0].strip()
+                value = pair[1].strip()
+                if not key or not value:
+                    continue
                 if key == "Status":
                     event.add(key.lower(), value)
-                elif key == "Level":
+                elif key == "level":
                     level = self.resolve_level(value)
                     if level:
                         event.add("description", level)
@@ -78,9 +89,9 @@ class SpyEyeCcBot(RSSBot):
                     if not self.treat_as_dns_source:
                         event.add("ip", value)
         event.add("feed", "abuse.ch")
-        event.add("malware", "SpyEye")
+        event.add("malware", "ZeuS")
         event.add("type", "c&c")
         return event
 
 if __name__ == "__main__":
-    SpyEyeCcBot.from_command_line().execute()
+    ZeusCcBot.from_command_line().execute()
