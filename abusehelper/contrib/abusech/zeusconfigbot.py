8c89a9d6548fa4616dd0d5f5a5cc029e72e93c90
Lari Huttunen <github@huttu.net>
2012-11-07 15:25:25 +0100

    abusehelper.contrib.abusech: Added *configbots to handle those specific feeds. Please note that the config URL feeds to not report IPs per se. The IP will be parsed from the URL if the URL is an  ip URL.

--- abusehelper/contrib/abusech/zeusconfigbot.py
+++ abusehelper/contrib/abusech/zeusconfigbot.py
@@ -1,5 +1,5 @@
 """
-SpyEye binary RSS feed bot.
+abuse.ch Zeus Config RSS feed bot.
 
 Maintainer: Lari Huttunen <mit-code@huttu.net>
 """
@@ -10,8 +10,8 @@ from abusehelper.core import bot, events
 from abusehelper.contrib.rssbot.rssbot import RSSBot
 
 
-class SpyEyeBinaryBot(RSSBot):
-    feeds = bot.ListParam(default=["https://spyeyetracker.abuse.ch/monitor.php?rssfeed=binaryurls"])
+class ZeusConfigBot(RSSBot):
+    feeds = bot.ListParam(default=["https://zeustracker.abuse.ch/monitor.php?urlfeed=configs"])
     # Please note that this feed does *not* report ips per se. They are derived
     # below from the URL if the URL is an ip URL. That is why the ip is never
     # dropped if it is present in the source even if the bool param
@@ -49,29 +49,26 @@ class SpyEyeBinaryBot(RSSBot):
                 pair = part.split(":", 1)
                 if len(pair) < 2:
                     continue
-
                 key = pair[0].strip()
                 value = pair[1].strip()
                 if not key or not value:
                     continue
-                if key == "SpyEye BinaryURL":
-                    proto = re.compile('^http:\/\/')
-                    url = proto.sub('hxxp://', value)
-                    event.add("url", url)
+                if key in ["Status", "version"]:
+                    event.add(key.lower(), value)
+                elif key == "URL":
+                    event.add("url", value)
                     parsed = urlparse.urlparse(value)
                     host = parsed.netloc
                     if self.is_ip(host):
                         event.add("ip", host)
                     else:
                         event.add("host", host)
-                elif key in ["Virustotal", "Status"]:
-                    event.add(key.lower(), value)
                 elif key == "MD5 hash":
                     event.add("md5", value)
         event.add("feed", "abuse.ch")
-        event.add("malware", "SpyEye")
-        event.add("type", "malware")
+        event.add("malware", "ZeuS")
+        event.add("type", "malware configuration")
         return event
 
 if __name__ == "__main__":
-    SpyEyeBinaryBot.from_command_line().execute()
+    ZeusConfigBot.from_command_line().execute()
