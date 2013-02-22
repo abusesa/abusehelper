b6c17a68e0ecd5d6a6810395fe24820cde94225d
Ossi Herrala <oherrala@gmail.com>
2013-02-22 12:08:04 +0000

    Rewrote the whole accesslog bot.
     * Return key-value from every item in access log.
     * Split request also into method, url and protocol key-value pairs.
     * Split user-agent string to software and version key-value pairs.

--- abusehelper/contrib/accesslogbot/accesslogbot.py
+++ abusehelper/contrib/accesslogbot/accesslogbot.py
@@ -1,6 +1,17 @@
 from abusehelper.core import bot, events
 from abusehelper.contrib.tailbot.tailbot import TailBot
 
+import time
+
+def convert_date(value):
+    try:
+        ts = time.strptime(value, "%d/%b/%Y:%H:%M:%S +0000")
+        ts = time.strftime("%Y-%m-%d %H:%M:%S UTC", ts)
+    except ValueError:
+        ts = value
+
+    return ts
+
 
 class AccessLogBot(TailBot):
     path = bot.Param("access_log file path")
@@ -10,24 +21,101 @@ class AccessLogBot(TailBot):
         if not line:
             return
 
-        left = line.split("[", 2)
-        ip, _, _ = left[0].split()
-        right = left[1].split("]", 2)
-        timestamp = right[0]
-        entry = right[1].split("\"")
-        request = entry[1]
-        status, bytes = entry[2].split()
-        user_agent = entry[5]
+        # LogFormat "%h %l %u %t \"%r\" %>s %b" common
+        # LogFormat "%h %l %u %t \"%r\" %>s %b \"%{Referer}i\" \"%{User-agent}i\"" combined
 
-        return events.Event({
-            "source": "accesslog",
-            "ip": ip,
-            "timestamp": timestamp,
-            "request": request,
-            "status": status,
-            "bytes": bytes,
-            "user_agent": user_agent
-        })
+        facts = {}
+
+        try:
+            left, _, right = line.partition(" ")
+            facts["ip"] = left.strip()
+            right = right.strip()
+
+            left, _, right = right.partition(" ")
+            if left != "-":
+                facts["ident"] = left.strip()
+                right = right.strip()
+
+            left, _, right = right.partition(" ")
+            if left != "-":
+                facts["user"] = left.strip()
+                right = right.strip()
+
+            if right.startswith("["):
+                left, _, right = right[1:].partition("]")
+                facts["timestamp"] = convert_date(left.strip())
+                right = right.strip()
+            else:
+                raise InputError("could not parse timestamp")
+
+            if right.startswith("\""):
+                left, _, right = right[1:].partition("\"")
+                facts["request"] = left.strip()
+                right = right.strip()
+            else:
+                raise InputError("could not parse request")
+
+            left, _, right = right.partition(" ")
+            if left != "-":
+                facts["status"] = left.strip()
+                right = right.strip()
+
+            left, _, right = right.partition(" ")
+            if left != "-":
+                facts["bytes"] = left.strip()
+                right = right.strip()
+
+            if right.startswith("\""):
+                left, _, right = right[1:].partition("\"")
+                if left != "-":
+                    facts["referer"] = left
+                right = right.strip()
+            else:
+                raise InputError("could not parse referer")
+
+            if right.startswith("\""):
+                left, _, right = right[1:].partition("\"")
+                if left != "-":
+                    facts["user_agent"] = left
+            else:
+                raise InputError("could not parse user_agent")
+
+        except InputError:
+            # All good
+            pass
+
+
+        if facts["request"]:
+            # Split request also into three parts
+            method, url, protocol = facts["request"].split(" ", 3)
+            facts["method"]   = method
+            facts["url"]      = url
+            facts["protocol"] = protocol
+
+
+        if facts["user_agent"]:
+            # Split user agent into software-version key-value pairs
+            uatemp = facts["user_agent"].strip()
+            facts["product"] = list()
+            comments = list()
+            while uatemp:
+                if uatemp.startswith("("):
+                    left, _, right = uatemp[1:].partition(")")
+                    comments.append(left.strip())
+                    uatemp = right.strip()
+                else:
+                    left, _, right = uatemp.strip().partition(" ")
+                    try:
+                        sw, version = left.split("/")
+                        if sw and version:
+                            facts[sw.lower()] = version
+                            facts["product"].append(sw+"/"+version)
+                    except ValueError:
+                        continue
+                    uatemp = right.strip()
+
+        return events.Event(facts)
+
 
 if __name__ == "__main__":
     AccessLogBot.from_command_line().execute()
