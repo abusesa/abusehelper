ff34a0b9ffc3fed7be9d30d65f9e8f0c24a3cf83
Sauli Pahlman <platypu5@users.noreply.github.com>
2013-08-26 00:20:28 +0300

    Make the bot to save memory by sending events as soon as it reads through the corresponding lines of the input file.

--- abusehelper/contrib/spamhaus/xbl.py
+++ abusehelper/contrib/spamhaus/xbl.py
@@ -15,7 +15,6 @@ class SpamhausXblBot(bot.PollingBot):
     def poll(self):
         skip_chars = ["#", ":", "$"]
         self.log.info("Opening %s" % self.xbl_filepath)
-        entries = []
 
         try:
             with open(self.xbl_filepath, "r") as f:
@@ -24,17 +23,13 @@ class SpamhausXblBot(bot.PollingBot):
                     if line and line[0] in skip_chars:
                         continue
 
-                    entries.append(line)
+                    event = events.Event()
+                    event.add("ip", line)
+                    event.add("description url", "http://www.spamhaus.org/query/bl?ip=" + line)
+                    yield idiokit.send(event)
 
-            self.log.info("Read %d entries" % len(entries))
         except IOError, ioe:
             self.log.error("Could not open %s: %s" % (self.xbl_filepath, ioe))
-
-        for entry in entries:
-            event = events.Event()
-            event.add("ip", entry)
-            event.add("description url", "http://www.spamhaus.org/query/bl?ip=" + entry)
-            yield idiokit.send(event)
 
 if __name__ == "__main__":
     SpamhausXblBot.from_command_line().execute()
