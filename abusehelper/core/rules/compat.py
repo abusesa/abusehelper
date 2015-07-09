ab9b24d38b29434b5623bf978a92f0b952af2e16
Joachim Viide <jviide@iki.fi>
2015-07-09 11:42:34 +0300

    abusehelper.core.rules.compat: Instantiating one of the deprecated classes (rules.AND, rules.OR, ...) now warns of their impending removal.

--- abusehelper/core/rules/compat.py
+++ abusehelper/core/rules/compat.py
@@ -1,5 +1,8 @@
 from __future__ import absolute_import
 
+import warnings
+import functools
+
 from . import atoms
 from . import rules
 
@@ -7,13 +10,29 @@ from . import rules
 __all__ = ["AND", "OR", "NOT", "MATCH", "ANYTHING", "NETBLOCK"]
 
 
-AND = rules.And
-OR = rules.Or
-NOT = rules.No
-MATCH = rules.Match
-ANYTHING = rules.Anything
+def _deprecated(callable):
+    @functools.wraps(callable)
+    def _callable(*args, **keys):
+        warnings.warn(
+            "abusehelper.core.rules.AND, OR, NOT, MATCH, ANYTHING and NETBLOCK " +
+            "are deprecated. They will be removed in an upcoming AbuseHelper version. " +
+            "Please use abusehelper.core.rules.And, Or, No, Match and Anything " +
+            "instead.",
+            DeprecationWarning,
+            stacklevel=2
+        )
+        return callable(*args, **keys)
+    return _callable
 
 
+AND = _deprecated(rules.And)
+OR = _deprecated(rules.Or)
+NOT = _deprecated(rules.No)
+MATCH = _deprecated(rules.Match)
+ANYTHING = _deprecated(rules.Anything)
+
+
+@_deprecated
 def NETBLOCK(ip_or_range, bits_or_end=None, keys=None):
     atom = atoms.IP(ip_or_range, bits_or_end)
     if keys:
