23138441e559d7f7634506a38b961102dda09764
Joachim Viide <jviide@iki.fi>
2016-01-06 22:28:43 +0200

    abusehelper.core.mail: Initial commit

---
+++ abusehelper/core/mail/README.md
@@ -0,0 +1,101 @@
+# Writing Maildir Bots
+
+The module ```maildirbot``` is the basis for writing Maildir bots. That is, bots that read files from a directory adhering to the [Maildir](https://en.wikipedia.org/wiki/Maildir) format and parsing events from the handled mails.
+
+The way Maildir bots are customized for your needs is a bit different from usual AbuseHelper workflow. ```maildirbot``` *does* provide a bot class called ```MailDirBot```, but you don't probably need to subclass it directly. Instead you write a custom subclass of ```maildirbot.Handler``` and provide that as a parameter when launching a ```MailDirBot``` instance.
+
+How and why follows.
+
+
+## Writing your own handler
+
+Everything starts by importing ```maildirbot``` and subclassing ```maildirbot.Handler```. Let's also import ```idiokit```, as we need it in a moment. Let's write the following to a file called ```myhandler.py```:
+
+```python
+import idiokit
+import maildirbot
+
+
+class MyHandler(maildirbot.Handler):
+    pass
+```
+
+In theory this is enough, yet useless. We need to figure out which mail parts we want to handle and their mime types. It's pretty straightforward: Defining a handler for mime type major/minor happens by defining a method called ```handle_major_minor```. For example text/plain parts would be handled by ```handle_text_plain```. This being AbuseHelper we need to decorate the method with ```idiokit.stream```.
+
+```python
+import idiokit
+import maildirbot
+
+
+class MyHandler(maildirbot.Handler):
+    @idiokit.stream
+    def handle_text_plain(self, ...):
+        ...
+```
+
+There is a notable exception when dealing with mime types containing hyphens such as application/octet-stream. In such cases replace the hyphen with a double underscore (e.g. ```handle_application_octet__stream```).
+
+So what does our ```handle_text_plain``` get as parameters? The complete method signature would be ```handle_text_plain(self, msg, log)``` where:
+   * ```msg``` is the currently handled part of the mail, wrapped in Python's standard ```email.message.Message``` object.
+   * ```log``` is regular logger object from Python's standard ```logging``` module.
+
+These parameters can in turn be used to parse AbuseHelper events from the mail part while logging interesting information about what's happening:
+
+```python
+import idiokit
+import maildirbot
+from abusehelper.core import events
+
+
+class MyHandler(maildirbot.Handler):
+    @idiokit.stream
+    def handle_text_plain(self, msg, log):
+        data = msg.get_payload(decode=True)
+
+        log.info("Starting to process {0} bytes of data".format(len(data)))
+
+        for line in data.splitlines():
+            yield idiokit.send(events.Event({
+                "line": line.decode("utf-8", "replace")
+            }))
+
+        log.info("Done")
+```
+
+And there you go, a *bona fide* handler. Now we just want to launch and test it somehow.
+
+
+## Launching a handler as a bot
+
+As stated earlier a custom handler should be provided as a parameter when launching a ```MailDirBot``` instance. This happens by adding the following to the end of your handler module:
+
+```python
+if __name__ == "__main__":
+    maildirbot.MailDirBot.from_command_line(MyHandler()).execute()
+```
+
+And there you go. You can now launch it with
+
+```
+$ python myhandler.py user@xmpp.example.com lobby.room Mail/customer1 myworkdir
+```
+
+and the bot will start chewing through the mails in the Mail/customer1 directory, producing the events from text/plain parts and moving the handled mails under the workdir. If you're feeling extra adventurous add ```--concurrency=5``` there and the bot will handle 5 mails simultaneously when it can.
+
+In startup configs the input mail directory is referred to as input_dir (```input_dir="Mail/customer1"```) and the work directory as work_dir (```work_dir="myworkdir"```).
+
+
+## Testing handlers
+
+All this doesn't really explain *why* we have to do this handler dance instead of just deriving directly from the ```maildirbot.MailDirBot``` as per usual.
+
+The motivation separating the part doing the Maildir juggling (```MailDirBot```) from the part actually interpreting the data (handlers) is that now you can test your handlers while writing them, provided that you have some suitable test data available in your development environment.
+
+Run your handler like this:
+
+```
+$ python -m maildirbot myhandler.MyHandler Mail/customer1/new
+```
+
+This will now parse the mails under the Mail/customer/new directory and output the events as JSON to STDOUT and write the logs to STDERR.
+Note that the mails from Mail/customer1/new aren't moved anywhere or deleted, as we're just testing.
---
+++ abusehelper/core/mail/maildirbot.py
@@ -0,0 +1,237 @@
+import os
+import uuid
+import fcntl
+import errno
+import idiokit
+import itertools
+import contextlib
+import email
+import email.header
+from abusehelper.core import bot, utils
+
+
+def try_rename(from_name, to_name):
+    try:
+        os.rename(from_name, to_name)
+    except OSError as ose:
+        if ose.errno != errno.ENOENT:
+            raise
+        return False
+    return True
+
+
+def try_read_message(path):
+    try:
+        with open(path, "rb") as fp:
+            return email.message_from_file(fp)
+    except IOError as ioe:
+        if ioe.errno != errno.ENOENT:
+            raise
+    return None
+
+
+def get_header(headers, key, default=None):
+    value = headers.get(key, None)
+    if value is None:
+        return default
+
+    bites = []
+    for string, encoding in email.header.decode_header(value):
+        if encoding is not None:
+            string = string.decode(encoding, "replace")
+        bites.append(string)
+
+    return u" ".join(bites)
+
+
+def makedirs(*args, **keys):
+    try:
+        os.makedirs(*args, **keys)
+    except OSError as ose:
+        if ose.errno != errno.EEXIST:
+            raise
+
+
+def iter_dir(dirname):
+    try:
+        dirlist = os.listdir(dirname)
+    except OSError as ose:
+        if ose.errno != errno.ENOENT:
+            raise
+        return
+
+    for filename in dirlist:
+        yield dirname, filename
+
+
+@contextlib.contextmanager
+def lockfile(filename):
+    with open(filename, "wb") as opened:
+        fd = opened.fileno()
+        try:
+            fcntl.flock(fd, fcntl.LOCK_EX | fcntl.LOCK_NB)
+        except IOError as ioe:
+            if ioe.errno not in (errno.EACCES, errno.EAGAIN):
+                raise
+            yield False
+        else:
+            try:
+                yield True
+            finally:
+                fcntl.flock(fd, fcntl.LOCK_UN)
+
+
+class Handler(object):
+    @idiokit.stream
+    def handle(self, msg, log):
+        handle_default = getattr(self, "handle_default", None)
+
+        for part in msg.walk():
+            content_type = part.get_content_type()
+            suffix = content_type.replace("-", "__").replace("/", "_")
+
+            handler = getattr(self, "handle_" + suffix, handle_default)
+            if handler is None:
+                continue
+
+            skip_rest = yield handler(part, log)
+            if skip_rest:
+                return
+
+
+class MailDirBot(bot.FeedBot):
+    input_dir = bot.Param()
+    work_dir = bot.Param()
+    concurrency = bot.IntParam(default=1)
+    poll_interval = bot.FloatParam(default=1)
+
+    def __init__(self, handler, *args, **keys):
+        bot.FeedBot.__init__(self, *args, **keys)
+
+        self._handler = handler
+        self._queue = utils.WaitQueue()
+
+    def feed_keys(self, *args, **keys):
+        for nth_concurrent in range(1, self.concurrency + 1):
+            yield (nth_concurrent,)
+
+    def run(self):
+        makedirs(self.work_dir)
+
+        with lockfile(os.path.join(self.work_dir, ".lock")) as success:
+            if not success:
+                self.log.error(u"Someone else is using the directory {0}".format(self.work_dir))
+            else:
+                bot.FeedBot.run(self)
+
+    @idiokit.stream
+    def _poll_files(self):
+        in_progress = os.path.join(self.work_dir, "in-progress")
+        done = os.path.join(self.work_dir, "done")
+
+        makedirs(in_progress)
+        makedirs(done)
+
+        for dirname, filename in iter_dir(in_progress):
+            input_name = os.path.join(dirname, filename)
+            output_name = os.path.join(done, filename)
+            yield idiokit.send(input_name, output_name)
+
+        while True:
+            paths = itertools.chain(
+                iter_dir(os.path.join(self.input_dir, "new")),
+                iter_dir(os.path.join(self.input_dir, "cur"))
+            )
+
+            for dirname, filename in paths:
+                uuid_name = uuid.uuid4().hex + "." + filename
+                input_name = os.path.join(in_progress, uuid_name)
+                output_name = os.path.join(done, uuid_name)
+                if try_rename(os.path.join(dirname, filename), input_name):
+                    yield idiokit.send(input_name, output_name)
+
+            yield idiokit.sleep(self.poll_interval)
+
+    @idiokit.stream
+    def _forward_files(self):
+        while True:
+            input_name, output_name = yield idiokit.next()
+
+            ack = idiokit.Event()
+            node = yield self._queue.queue(0, (input_name, output_name, ack))
+            try:
+                yield ack
+            finally:
+                yield self._queue.cancel(node)
+
+    @idiokit.stream
+    def main(self, state):
+        yield self._poll_files() | self._forward_files()
+
+    @idiokit.stream
+    def feed(self, nth_concurrent):
+        while True:
+            input_name, output_name, ack = yield self._queue.wait()
+            ack.succeed()
+
+            msg = try_read_message(input_name)
+            if msg is None:
+                continue
+
+            subject = get_header(msg, "Subject", "<no subject>")
+            sender = get_header(msg, "From", "<unknown sender>")
+            self.log.info(u"Handler #{0} handling mail '{1}' from {2}".format(nth_concurrent, subject, sender))
+
+            yield self._handler.handle(msg, self.log)
+
+            os.rename(input_name, output_name)
+            self.log.info(u"Handler #{0} done with mail '{1}' from {2}".format(nth_concurrent, subject, sender))
+
+
+if __name__ == "__main__":
+    import json
+    import logging
+    import optparse
+
+    logging.basicConfig(
+        level=logging.DEBUG,
+        format="%(asctime)s %(levelname)s %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%SZ"
+    )
+
+    @idiokit.stream
+    def print_events():
+        while True:
+            event = yield idiokit.next()
+
+            json_dict = {}
+            for key in event.keys():
+                values = event.values(key)
+                if len(values) == 1:
+                    json_dict[key] = values[0]
+                else:
+                    json_dict[key] = list(values)
+            print json.dumps(json_dict)
+
+    parser = optparse.OptionParser()
+    parser.set_usage("usage: %prog [options] handler [dirname ...]")
+
+    options, args = parser.parse_args()
+
+    if len(args) < 1:
+        parser.error("expected handler")
+    module, _, classname = args[0].rpartition(".")
+    mod = __import__(module, fromlist=[classname])
+    handler = getattr(mod, classname)()
+
+    for arg in args[1:]:
+        for dirname, filename in iter_dir(arg):
+            orig_name = os.path.join(dirname, filename)
+            msg = try_read_message(orig_name)
+            if msg is None:
+                logging.info("skipped '{0}' (could not open)".format(orig_name))
+                continue
+
+            logging.info("handling '{0}'".format(orig_name))
+            idiokit.main_loop(handler.handle(msg, logging) | print_events())
+            logging.info("done with '{0}'".format(orig_name))
