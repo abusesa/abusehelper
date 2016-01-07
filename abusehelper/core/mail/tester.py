e504e4b0c293143ca27011bc4ac96b9726a058b6
Joachim Viide <jviide@iki.fi>
2016-01-08 00:08:02 +0200

    abusehelper.core.mail.tester: Add handle(...) to facilititate handler testing

--- abusehelper/core/mail/tester.py
+++ abusehelper/core/mail/tester.py
@@ -2,6 +2,7 @@ import os
 import json
 import email
 import logging
+import inspect
 import optparse
 
 import idiokit
@@ -9,19 +10,88 @@ from ..utils import format_exception
 from ._utils import load_callable
 
 
+def _event_to_dict(event):
+    event_dict = {}
+    for key in event.keys():
+        values = event.values(key)
+        if len(values) == 1:
+            event_dict[key] = values[0]
+        else:
+            event_dict[key] = list(values)
+    return event_dict
+
+
+@idiokit.stream
+def _collect_events():
+    results = []
+    while True:
+        try:
+            event = yield idiokit.next()
+        except StopIteration:
+            idiokit.stop(results)
+        else:
+            results.append(_event_to_dict(event))
+
+
 @idiokit.stream
 def _print_events():
     while True:
         event = yield idiokit.next()
+        print json.dumps(_event_to_dict(event))
 
-        json_dict = {}
-        for key in event.keys():
-            values = event.values(key)
-            if len(values) == 1:
-                json_dict[key] = values[0]
-            else:
-                json_dict[key] = list(values)
-        print json.dumps(json_dict)
+
+class _NullHandler(logging.Handler):
+    r"""
+    A dummy logging handler that just scraps the log messages.
+
+    Needed because of Python 2.6, as the Python 2.7 standard library
+    contains logging.handlers.NullHandler.
+    """
+
+    def emit(self, record):
+        pass
+
+
+def handle(handler_class, msg_data):
+    r"""
+    Return a list of event dictionaries collected by handling
+    msg_data using handler_class.
+
+    >>> from abusehelper.core.events import Event
+    >>> from abusehelper.core.mail import Handler
+    >>>
+    >>> class MyHandler(Handler):
+    ...     @idiokit.stream
+    ...     def handle_text_plain(self, msg):
+    ...         yield idiokit.send(Event(a="test"))
+    ...
+    >>> handle(MyHandler, "From: test@email.example\n\nThis is the payload.")
+    [{u'a': u'test'}]
+
+    Note that to simplify testing the output is a list of dictionaries
+    instead of abusehelper.core.events.Event objects.
+
+    msg_data will be cleaned using inspect.cleandoc, so the previous example
+    can be expressed with triple quotes:
+
+    >>> handle(MyHandler, '''
+    ...     From: test@email.example
+    ...
+    ...     This is the payload.
+    ... ''')
+    [{u'a': u'test'}]
+    """
+
+    msg = email.message_from_string(inspect.cleandoc(msg_data))
+
+    log = logging.getLogger("Null")
+    log_handler = _NullHandler()
+    log.addHandler(log_handler)
+    try:
+        handler = handler_class(log)
+        return idiokit.main_loop(handler.handle(msg) | _collect_events())
+    finally:
+        log.removeHandler(log_handler)
 
 
 def main():
---
+++ abusehelper/core/mail/tests/test_tester.py
@@ -0,0 +1,42 @@
+import idiokit
+import unittest
+
+from ...events import Event
+from .. import Handler
+from ..tester import handle
+
+
+class TestTester(unittest.TestCase):
+    def test_strings_should_get_handled(self):
+        class PayloadLineHandler(Handler):
+            @idiokit.stream
+            def handle_text_plain(self, msg):
+                data = msg.get_payload()
+                for line in data.splitlines():
+                    yield idiokit.send(Event(line=line))
+                idiokit.stop(True)
+
+        self.assertEqual(
+            handle(PayloadLineHandler, """
+                From: test@email.example
+
+                This is the payload.
+            """),
+            [{"line": "This is the payload."}]
+        )
+
+    def test_exceptions_raised_inside_the_handler_should_get_propagated(self):
+        class HandlerFailure(Exception):
+            pass
+
+        class FailingHandler(Handler):
+            @idiokit.stream
+            def handle_text_plain(self, msg):
+                yield idiokit.sleep(0.0)
+                raise HandlerFailure()
+
+        self.assertRaises(HandlerFailure, handle, FailingHandler, """
+            From: test@email.example,
+
+            This is the payload.
+        """)
