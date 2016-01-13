a0c8b269aa81697148b3c72f73e7c182ef67ce1a
Joachim Viide <jviide@iki.fi>
2016-01-14 01:23:42 +0200

    abusehelper.core.rules._domainname: Add parse_name

--- abusehelper/core/rules/_domainname.py
+++ abusehelper/core/rules/_domainname.py
@@ -1,4 +1,70 @@
+import re
 from itertools import izip
+from encodings import idna
+
+
+# A regular expression for checking a validity of a ASCII domain name label.
+_LABEL_REX = re.compile(r"^[a-z0-9](?:[\-a-z0-9]{0,61}[a-z0-9])?$")
+
+
+def parse_name(string):
+    r"""
+    Return domain name as a tuple of unicode labels parsed from the given
+    unicode string.
+
+    >>> parse_name(u"domain.example")
+    (u'domain', u'example')
+
+    Return None if the string is not a well-formed domain name. For example the
+    name must honor the maximum name length (253 characters, including periods)
+    and label length limits (1-63 characters).
+
+    >>> parse_name(u"a." * 128 + u"example")
+    >>> parse_name(u"a" * 64 + u".example")
+    >>> parse_name(u"domain..example")
+
+    After unicode labels have been run through the IDNA ToASCII transformation
+    additional restrictions outlined in RFC 3696, section 2 are applied:
+
+      * Labels must consist only of ASCII alphabetic characters
+
+        >>> parse_name(u"a domain.example")
+
+      * A hyphen is not permitted to appear at either the beginning or end of a label.
+
+        >>> parse_name(u"-domain.example")
+        >>> parse_name(u"domain-.example")
+
+      * The a top-level domain should not be all-numeric.
+
+        >>> parse_name(u"domain.example.123")
+
+    As a special limitation plain top-level domains are not accepted.
+
+    >>> parse_name(u"example")
+    """
+
+    labels = string.split(".")
+
+    # Don't accept plain top-level domains.
+    if len(labels) < 2:
+        return None
+
+    try:
+        # Note: idna.ToASCII also enforces the minimum and maximum label length.
+        labels = map(idna.ToASCII, string.split("."))
+    except UnicodeError:
+        return None
+
+    if len(labels) + sum(len(x) for x in labels) > 253:
+        return None
+
+    if labels[-1].isdigit():
+        return None
+    if not all(_LABEL_REX.match(x) for x in labels):
+        return None
+
+    return tuple(idna.ToUnicode(x) for x in labels)
 
 
 def _issubdomain(name, pattern_labels):
