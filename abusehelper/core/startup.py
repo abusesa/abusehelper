1bf2906d4bbed8fdc7d14f9d421f5e76acf5641d
Joachim Viide <jviide@iki.fi>
2016-03-10 00:27:58 +0200

    Rewrite core.startup to be more easily extendable

--- abusehelper/core/startup.py
+++ abusehelper/core/startup.py
@@ -2,16 +2,15 @@ from __future__ import absolute_import
 
 import os
 import sys
-import time
 import errno
 import signal
 import numbers
+import inspect
 import subprocess
 import cPickle as pickle
-from numbers import Real
 
 import idiokit
-from . import bot, config
+from . import bot, config, utils
 
 
 def iter_startups(iterable):
@@ -22,30 +21,44 @@ def iter_startups(iterable):
             continue
 
 
-def _signal_number_to_symbols(signal_number):
+def _signal_numbers_to_names():
+    signums = {}
 
-    signal_names = []
-
-    for name in dir(signal):
-
+    for name, value in inspect.getmembers(signal):
         if not name.startswith("SIG") or name.startswith("SIG_"):
             continue
-
-        value = getattr(signal, name)
 
         if not isinstance(value, numbers.Integral):
             continue
 
-        if not value == signal_number:
-            continue
+        signums.setdefault(value, []).append(name)
 
-        signal_names.append(name)
+    for signum, names in signums.items():
+        signums[signum] = tuple(sorted(names))
 
-    return signal_names
+    return signums
+
+
+_signums = _signal_numbers_to_names()
+
+
+def _signal_number_to_name(signum):
+    """
+    Return a name for the signal number, or None if no name can be found.
+
+    >>> _signal_number_to_name(signal.SIGINT)
+    'SIGINT'
+    >>> _signal_number_to_name(signal.NSIG + 1)
+    """
+
+    names = _signums.get(signum, [])
+    if not names:
+        return None
+    return "/".join(names)
 
 
 class Bot(object):
-    _defaults = dict()
+    _defaults = {}
 
     @classmethod
     def template(cls, *args, **keys):
@@ -108,32 +121,155 @@ class Bot(object):
         return result if result is NotImplemented else not result
 
 
-def kill(process, signum):
+def _kill(process, signum):
     try:
         os.kill(process.pid, signum)
-    except OSError, ose:
+    except OSError as ose:
         if ose.errno != errno.ESRCH:
             raise
+
+
+class _ConfSignal(Exception):
+    def __init__(self, signum):
+        Exception.__init__(self, signum)
+
+    @property
+    def signum(self):
+        return self.args[0]
 
 
 class StartupBot(bot.Bot):
     def __init__(self, *args, **keys):
         bot.Bot.__init__(self, *args, **keys)
 
-        self._strategies = dict()
-        self._processes = dict()
-        self._updated = None
+        self._handlers = {}
+        self._processes = {}
 
     @idiokit.stream
     def configs(self):
         yield idiokit.sleep(0.0)
 
-    def strategy(self, conf, delay=15):
+    @idiokit.stream
+    def handle(self, conf, delay=15):
         while True:
-            yield conf
+            self.log.info("Launching bot {0!r} from module {1!r}".format(conf.name, conf.module))
+            yield self.launch(conf)
 
-            self.log.info("Relaunching %r in %d seconds", conf.name, delay)
-            yield delay
+            self.log.info("Relaunching {0!r} in {1} seconds".format(conf.name, delay))
+            yield idiokit.sleep(delay)
+
+    @idiokit.stream
+    def launch(self, conf):
+        if conf in self._processes:
+            raise RuntimeError("can not launch two processes with same conf")
+
+        process = self._launch(conf)
+        self._processes[conf] = process
+
+        sig = None
+
+        while self._poll(conf, process) is None:
+            try:
+                yield idiokit.sleep(0.25)
+            except _ConfSignal as sig:
+                siginfo = "signal {0}".format(sig.signum)
+                signame = _signal_number_to_name(sig.signum)
+                if signame is not None:
+                    siginfo += " (" + signame + ")"
+                self.log.info("Sending {0} to {1!r}".format(siginfo, conf.name))
+                _kill(process, sig.signum)
+
+        if sig is not None:
+            raise sig
+        idiokit.stop(process.returncode)
+
+    def main(self):
+        error_event = idiokit.Event()
+        listen = self._listen_configs(error_event)
+        idiokit.pipe(error_event, listen)
+        return idiokit.pipe(self.configs(), listen)
+
+    def run(self):
+        return idiokit.main_loop(self.main())
+
+    @idiokit.stream
+    def _listen_configs(self, error_event):
+        closing = False
+        term_count = 0
+        configs = frozenset()
+
+        try:
+            while True:
+                try:
+                    while not closing:
+                        if frozenset(self._handlers) == configs:
+                            configs = yield idiokit.next()
+                            configs = frozenset(iter_startups(config.flatten(configs)))
+                        yield self._apply(configs, error_event)
+                    yield self._wait(self._handlers.values())
+                except idiokit.Signal as sig:
+                    closing = True
+
+                    if sig.signum == signal.SIGUSR1:
+                        self._clean(signal.SIGTERM)
+                        continue
+
+                    if sig.signum == signal.SIGUSR2:
+                        self._clean(signal.SIGKILL)
+                        continue
+
+                    if term_count == 0:
+                        self._clean(signal.SIGTERM)
+                        term_count += 1
+                        continue
+                break
+        finally:
+            self._check()
+
+    @idiokit.stream
+    def _apply(self, configs, error_event):
+        removed = frozenset(self._handlers).difference(configs)
+        added = frozenset(configs).difference(self._handlers)
+
+        for conf in removed:
+            handler = self._handlers[conf]
+            handler.throw(_ConfSignal(signal.SIGTERM))
+
+        yield self._wait([self._handlers[conf] for conf in removed])
+
+        for conf in removed:
+            del self._handlers[conf]
+
+        for conf in added:
+            self._handlers[conf] = self._wrap_handler(self.handle(conf), error_event)
+
+    @idiokit.stream
+    def _wrap_handler(self, handler, error_event):
+        try:
+            yield handler
+        except _ConfSignal:
+            pass
+        except:
+            error_event.fail()
+
+    @idiokit.stream
+    def _wait(self, handlers):
+        for handler in handlers:
+            yield idiokit.pipe(idiokit.Event(), handler)
+
+    def _clean(self, signum):
+        for conf, handler in self._handlers.items():
+            handler.throw(_ConfSignal(signum))
+
+    def _check(self):
+        for conf, process in self._processes.items():
+            self._poll(conf, process)
+
+        if self._processes:
+            self.log.info("{0} bot(s) left alive: {1}".format(
+                len(self._processes),
+                ", ".join("{0!r}[{1}]".format(conf.name, process.pid) for (conf, process) in self._processes.items())
+            ))
 
     def _launch(self, conf):
         args = [sys.executable]
@@ -151,148 +287,49 @@ class StartupBot(bot.Bot):
         env = dict(os.environ)
         env["ABUSEHELPER_CONF_FROM_STDIN"] = "1"
         try:
-            process = subprocess.Popen(args, cwd=conf.workdir, env=env, stdin=subprocess.PIPE, close_fds=True)
-        except OSError, ose:
-            self.log.error("Failed launching bot %r: %r", conf.name, ose)
+            process = subprocess.Popen(
+                args,
+                cwd=conf.workdir,
+                env=env,
+                stdin=subprocess.PIPE,
+                close_fds=True
+            )
+        except OSError as ose:
+            self.log.error("Failed launching bot {0!r} ({1})".format(
+                conf.name,
+                utils.format_exception(ose))
+            )
             return None
 
         try:
             pickle.dump(conf.params, process.stdin)
             process.stdin.flush()
-        except IOError, ioe:
-            self.log.error("Failed sending configuration to bot %r: %r", conf.name, ioe)
+        except IOError as ioe:
+            self.log.error("Failed sending configuration to bot {0!r} ({1})".format(
+                conf.name,
+                utils.format_exception(ioe))
+            )
 
         return process
 
-    def _poll(self):
-        for conf, (process, strategy) in list(self._processes.iteritems()):
-            if process is not None and process.poll() is None:
-                continue
-            if process is not None and process.poll() is not None:
-                logline = "Bot %r was terminated." % (conf.name)
+    def _poll(self, conf, process):
+        if process.poll() is None:
+            return None
 
-                if process.returncode < 0:
-                    logline += " Terminated by signal %d" % (process.returncode)
-                    signames = _signal_number_to_symbols(abs(process.returncode))
-                    if signames:
-                        logline += " (%s)" % " or ".join(signames)
-                else:
-                    logline += " Return code %d" % (process.returncode)
+        if self._processes.get(conf, None) is process:
+            del self._processes[conf]
 
-                self.log.info(logline)
-
-            self._processes.pop(conf, None)
-            self._strategies[conf] = time.time(), strategy
-
-    def _purge(self):
-        now = time.time()
-        for conf, (expiration, strategy) in list(self._strategies.iteritems()):
-            if expiration > now:
-                continue
-
-            try:
-                output_value = strategy.next()
-            except StopIteration:
-                del self._strategies[conf]
-                continue
-
-            if isinstance(output_value, Real):
-                self._strategies[conf] = output_value + now, strategy
+            if process.returncode >= 0:
+                info = "with return code {0}".format(process.returncode)
             else:
-                del self._strategies[conf]
-                yield output_value, strategy
+                signum = -process.returncode
+                info = "by signal {0}".format(signum)
+                signame = _signal_number_to_name(signum)
+                if signame is not None:
+                    info += " (" + signame + ")"
 
-    def _close(self):
-        for _, strategy in self._strategies.itervalues():
-            strategy.close()
-        self._strategies.clear()
-
-    def _clean(self, signame, signum):
-        self._poll()
-        self._close()
-        if not self._processes:
-            return
-
-        self.log.info("Sending %s to alive bots" % (signame,))
-        for conf, (process, _) in self._processes.iteritems():
-            kill(process, signum)
-
-    @idiokit.stream
-    def read(self):
-        try:
-            while True:
-                configs = yield idiokit.next()
-                self._updated = set(iter_startups(config.flatten(configs)))
-        finally:
-            self._updated = None
-
-    @idiokit.stream
-    def main(self, poll_interval=0.25):
-        try:
-            discard = set()
-            closing = False
-            term_count = 0
-
-            while True:
-                try:
-                    yield idiokit.sleep(poll_interval)
-                except idiokit.Signal as sig:
-                    signum = sig.args[0]
-                    closing = True
-
-                    if signum == signal.SIGUSR1:
-                        self._clean("SIGTERM", signal.SIGTERM)
-                    elif signum == signal.SIGUSR2:
-                        self._clean("SIGKILL", signal.SIGKILL)
-                    elif term_count == 0:
-                        self._clean("SIGTERM", signal.SIGTERM)
-                        term_count += 1
-                    else:
-                        return
-
-                self._poll()
-
-                if closing:
-                    self._close()
-                    if not self._strategies and not self._processes:
-                        return
-                    continue
-
-                if self._updated is not None:
-                    current = set(self._processes) | set(self._strategies)
-
-                    for conf in (current - self._updated) - discard:
-                        if conf in self._processes:
-                            process, strategy = self._processes[conf]
-                            self.log.info("Sending SIGTERM to %r", conf.name)
-                            kill(process, signal.SIGTERM)
-                        discard.add(conf)
-
-                    for conf in self._updated - current:
-                        self._strategies[conf] = time.time(), self.strategy(conf)
-
-                    self._updated = None
-
-                if discard:
-                    for conf in discard.intersection(self._strategies):
-                        _, strategy = self._strategies.pop(conf)
-                        strategy.close()
-                        discard.discard(conf)
-                    continue
-
-                for conf, strategy in self._purge():
-                    self.log.info("Launching bot %r from module %r", conf.name, conf.module)
-                    self._processes[conf] = self._launch(conf), strategy
-        finally:
-            self._poll()
-            if self._processes:
-                info = ", ".join(
-                    "%r[%d]" % (conf.name, process.pid)
-                    for (conf, (process, _)) in self._processes.iteritems())
-                self.log.info("%d bot(s) left alive: %s" % (len(self._processes), info))
-
-    def run(self):
-        return idiokit.main_loop(self.configs() | self.read() | self.main())
+            self.log.info("Bot {0!r} was terminated {1}".format(conf.name, info))
+        return process.returncode
 
 
 class DefaultStartupBot(StartupBot):
