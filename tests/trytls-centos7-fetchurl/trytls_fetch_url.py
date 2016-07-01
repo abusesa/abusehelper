72b5f14aeff0af199c5da634d372cc2026513190
Ossi Herrala <oherrala@gmail.com>
2016-07-02 01:12:49 +0300

    Try to cut code size. Also fix return values TryTLS excepts.

--- tests/trytls-centos7-fetchurl/trytls_fetch_url.py
+++ tests/trytls-centos7-fetchurl/trytls_fetch_url.py
@@ -6,34 +6,24 @@ from abusehelper.core.utils import fetch_url, FetchUrlFailed
 from idiokit.ssl import SSLCertificateError
 
 
-def main():
-    if len(sys.argv) < 3:
-        print "{0} <host> <port> [ca-bundle]".format(sys.argv[0])
-        sys.exit(1)
-
-    host = sys.argv[1]
-    port = sys.argv[2]
-    ca_bundle = None
-    if len(sys.argv) == 4:
-        ca_bundle = sys.argv[3]
-
+def main(host, port, ca_bundle=True):
     url = "https://{0}:{1}/".format(host, port)
 
     try:
-        if ca_bundle:
-            idiokit.main_loop(fetch_url(url, verify=ca_bundle))
-        else:
-            idiokit.main_loop(fetch_url(url))
+        idiokit.main_loop(fetch_url(url, verify=ca_bundle))
     except FetchUrlFailed:
-        print "VERIFY SUCCESS"
-        sys.exit(0)
+        print "VERIFY FAILURE"
     except SSLCertificateError:
+        print "VERIFY FAILURE"
+    else:
         print "VERIFY SUCCESS"
-        sys.exit(0)
 
-    print "VERIFY FAILURE"
     sys.exit(0)
 
 
 if __name__ == '__main__':
-    main()
+    args = tuple(sys.argv[1:])
+    try:
+        main(*args)
+    except TypeError:
+        print "{0} <host> <port> [ca-bundle]".format(sys.argv[0])
